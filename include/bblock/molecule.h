115c87a8677412d374cf7f60ee94b25eb0970248
Marc Riera <mrierari@ucsd.edu>
2016-04-28 22:19:37 -0700

    Modified tree on include
    Energy compiles but does not work. Need to fix.

---
+++ include/bblock/molecule.h
@@ -0,0 +1,39 @@
+#ifndef MOLECULE_H
+#define MOLECULE_H
+
+#include <vector>
+#include <string>
+
+#include "monomer.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace bblock { // Building Block :: molecule
+
+////////////////////////////////////////////////////////////////////////////////
+
+class molecule {
+
+  public:
+    molecule();
+    ~molecule();
+    void clear();
+    
+    int get_n_mon();
+    void add_monomer(std::string mon_name, double * xyz,
+          std::vector<std::string> names);
+//    monomer * get_monomer(size_t n);
+//    monomer * get_monomer();
+
+  private:
+    int n_mon;
+    std::vector<monomer> monomers;
+    
+    
+};
+
+} // namespace bblock
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // MOLECULE_H
