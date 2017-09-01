8c35290d825daf509b3d3fa4af24d21b816dd9a8
Marc Riera <mrierari@ucsd.edu>
2017-09-01 09:09:07 -0700

    [add] Test case for water monomer energy with gradients

--- test/src-test/energy-test.cpp
+++ test/src-test/energy-test.cpp
@@ -13,7 +13,7 @@
 #include "system.h"
 
 
-//#define PRINT_GRADS
+#define PRINT_GRADS
 namespace {
 
 static std::vector<bblock::System> systems;
@@ -44,6 +44,7 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  std::cout << "Energies without gradients:" << std::endl;
   for (size_t i = 0; i < systems.size(); i++) {
     double energy = systems[i].Energy();
     std::cout << std::setprecision(10) << std::scientific
@@ -62,8 +63,15 @@ int main(int argc, char** argv)
               << "system["  << std::setfill('.')
               << std::setw(5) << i << "]= " << std::setfill(' ')
               << std::setw(20) << std::right << energy
-              << std::setw(12) << std::right << " kcal/mol" << std::endl;
-    std::vector<std::string> atn = systems[i].GetAtomNames();
+              << std::setw(12) << std::right << " kcal/mol" 
+              << std::endl << std::endl;
+    std::vector<std::string> atn = systems[i].GetSysAtNames();
+
+    std::cout << std::setw(6)  << std::left << "Atom"
+              << std::setw(20) << std::right << "GradientX"
+              << std::setw(20) << std::right << "GradientY"
+              << std::setw(20) << std::right << "GradientZ"
+              << std::endl;
     for (size_t j = 0; j < n_sites; j++) {
       std::cout << std::setprecision(10) << std::scientific
                 << std::setw(6) << std::left << atn[j]
@@ -72,32 +80,32 @@ int main(int argc, char** argv)
                 << std::setw(20) << std::right << grd[3*j + 2]
                 << std::endl;
     }
-    std::cout << std::setw(6) << "Atom"
-              << std::setw(20) << "Analytical"
-              << std::setw(20) << "Numerical"
-              << std::setw(20) << "Difference"
+    std::cout << std::endl << std::setw(6)  << std::left << "Atom"
+              << std::setw(20) << std::right << "Analytical"
+              << std::setw(20) << std::right << "Numerical"
+              << std::setw(20) << std::right << "Difference"
               << std::endl;
     // Comparing analytical and numerical
-    double xyz[n_sites * 3];
-    systems[i].GetXyz(xyz);
+    std::vector<double> xyz;
+    xyz = systems[i].GetSysXyz();
     const double eps = 1.0e-6;
     for (size_t j = 0; j < n_sites * 3; j++) {
       const double x_orig = xyz[j];
       xyz[j] = x_orig + eps;
-      systems[i].SetXyz(xyz);
+      systems[i].SetSysXyz(xyz);
       const double Ep = systems[i].Energy();
       xyz[j] = x_orig + 2 * eps;
-      systems[i].SetXyz(xyz);
+      systems[i].SetSysXyz(xyz);
       const double Epp = systems[i].Energy();
       xyz[j] = x_orig - eps;
-      systems[i].SetXyz(xyz);
+      systems[i].SetSysXyz(xyz);
       const double Em = systems[i].Energy();
       xyz[j] = x_orig - 2 * eps;
-      systems[i].SetXyz(xyz);
+      systems[i].SetSysXyz(xyz);
       const double Emm = systems[i].Energy();
       const double gfd = (8*(Ep - Em) - (Epp - Emm))/(12*eps);
       xyz[j] = x_orig;
-      systems[i].SetXyz(xyz);
+      systems[i].SetSysXyz(xyz);
       std::cout << std::setprecision(10) << std::scientific
                 << std::setw(6) << std::left << atn[j/3]
                 << std::setw(20) << std::right << grd[j]
