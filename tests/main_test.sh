76133274707ab1fb02d29160701171feb93c936c
Marc Riera <mrierari@ucsd.edu>
2017-09-05 19:25:20 -0700

    [add] flag in main test script to ensure code compiled

--- tests/main_test.sh
+++ tests/main_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "../bin" ]; then
+  echo "Please compile the code first. Exiting now..."
+  exit
+fi
+
 ./run_tests.sh > run_tests.out
 if ! grep -q "FAILED" run_tests.out ; then
   exit 0
