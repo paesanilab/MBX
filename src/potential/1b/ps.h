bd73450c59de45bf79880149097eb32812f50f8d
Marc Riera <mrierari@ucsd.edu>
2017-09-06 17:10:48 -0700

    [add] function ready to be vectorized

--- src/potential/1b/ps.h
+++ src/potential/1b/ps.h
@@ -6,6 +6,7 @@ namespace ps {
 double pot_nasa(const double*, double*);
 void dms_nasa(const double&, const double&, const double&,
               const double*, double* q3, double* dq3, bool ttm3);
+double pot_nasa(const double*, double*, size_t);
 
 // r1 = r2 = 0.9587, \theta = 104.3850
 const double e_zero = 0.0; // TODO: fix this
