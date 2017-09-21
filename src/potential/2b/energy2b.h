61ac70c2b9be126987c23733a78eba5052f8f055
Marc Riera <mrierari@ucsd.edu>
2017-09-21 10:36:30 -0700

    [add] new cpp/h files that will take care of deciding the polynomials to use. This way the system class will be more general.

---
+++ src/potential/2b/energy2b.h
@@ -0,0 +1,21 @@
+#ifndef ENERGY2B_H
+#define ENERGY2B_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+// 2B
+#include "potential/2b/x2b-v9x.h"
+#include "potential/2b/poly-2b-v6x.h"
+
+namespace e2b {
+
+double get_2b_energy(std::string m1, std::string m2, size_t nm,
+                     std::vector<double> xyz1, std::vector<double> xyz2);
+double get_2b_energy(std::string m1, std::string m2,size_t nm, 
+                     std::vector<double> xyz1, std::vector<double> xyz2
+                     std::vector<double> &grd1, std::vector<double> &grd2);
+
+} // namespace e2b
+#endif
