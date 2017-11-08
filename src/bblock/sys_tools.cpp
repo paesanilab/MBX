7427bec61c311566b79a45780b7e3e89e8d78787
Bella Le <le.bella98@gmail.com>
2017-11-08 14:06:49 -0800

    sys_tools

--- src/bblock/sys_tools.cpp
+++ src/bblock/sys_tools.cpp
@@ -329,31 +329,26 @@ void SetCharges (std::vector<double> xyz, std::vector<double> &charges,
     std::vector<double> chgtmp;
     size_t fstind_3 = 3*fst_ind;
     
-    chg_der = std::vector<double>(27*n_mon, 0.0);
+    std::vector <double> chgtmpnv_der (3*n_mon + 27*n_mon, 0.0);
 
-    // TODO NOT VECTORIZED because of creation of vector objects within loop
-    // However vector objects currently needed because dms_nasa can only
-    // calculate charge for 1 atom at a time
     // Calculate individual monomer's charges
-    for (size_t nv = 0; nv < n_mon; nv++) {
+//    for (size_t nv = 0; nv < n_mon; nv++) {
 
       size_t ns3 = nsites*3;
-      size_t shift = 27*nv;
+//      size_t shift = 27*nv;
       
       // Getting front and end of xyz vector of 1 monomer in system
-      std::vector<double>::const_iterator first = xyz.begin()+(nv*ns3)+fstind_3;
-      std::vector<double>::const_iterator end = 
-                                             xyz.begin()+((nv+1)*ns3)+fstind_3;
-      std::vector<double> atomcoords(first, end);
-      std::vector<double> chgtmpnv((nsites-1));
+      std::vector<double> atomcoords(xyz);
+//      std::vector<double> chgtmpnv((nsites-1));
 
       // Calculating charge
-      ps::dms_nasa (0.0, 0.0, 0.0, atomcoords.data(), 
-                    chgtmpnv.data(), chg_der.data() + shift, false);
+      ps::dms_nasa (0.0, 0.0, 0.0, atomcoords.data(), // + (nv*ns3)+fstind_3, 
+                    chgtmpnv_der.data(), mon_id, n_mon, fst_ind, false);
+                    //, chg_der.data() + shift, false);
       // Inserting the found charges into chgtmp vector before calculating
       // new charge values
-      chgtmp.insert (chgtmp.end(), chgtmpnv.begin(), chgtmpnv.end());
-    }
+      chgtmp.insert (chgtmp.end(), chgtmpnv_der.begin(), chgtmpnv_der.end());
+//    }
 
     // Constant that calculates charge
     const double CHARGECON = constants::CHARGECON;
