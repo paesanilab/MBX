abe7b19bb3a3df9dde928cdb90198af3af5fa5da
Marc Riera <mrierari@ucsd.edu>
2017-12-09 16:21:33 -0800

    fixed typo in test 1

--- tests/tests/test_001_io.sh
+++ tests/tests/test_001_io.sh
@@ -3,5 +3,7 @@
 filename=$(basename "$0")
 filename="${filename%.*}"
 
+mkdir -p outputs
+
 ../../build/stage/usr/local/clusters_ultimate/bin/io-test inputs/${filename}.nrg outputs/${filename}.out 
 
