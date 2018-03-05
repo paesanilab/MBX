d5b86ae2d03b573edf19040063bca48457712941
Marc Riera <mrierari@ucsd.edu>
2018-03-05 11:22:11 -0800

    CHanged compilation script to remove the install folder before doing a clean install

--- compile.sh
+++ compile.sh
@@ -5,14 +5,14 @@ if [ $# -ne 1 ]; then
 fi
 
 if [ "$1" == "gnu" ]; then
-  rm -rf build bin
-  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=" -O0 -Wall -ftree-vectorize -ftree-vectorizer-verbose=2" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -H. -Bbuild
+  rm -rf build install
+  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=" -fPIC -O0 -Wall -ftree-vectorize -ftree-vectorizer-verbose=2" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -H. -Bbuild
   cd build
   make --no-print-directory CXX=g++ CC=gcc
   cd ../
 
 else
-  rm -rf build bin
+  rm -rf build install
   cmake  -DCMAKE_CXX_FLAGS="-Wall -qopt-report " -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc -H. -Bbuild
   cd build
   make CXX=icpc CC=icc 
