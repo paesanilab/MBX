f9aa31e2f4cfc9df042fbb103a6f9a360b1ab926
Marc Riera <mrierari@ucsd.edu>
2018-03-28 13:18:24 -0700

    Updated tests. Removed part of the code that prints XYZ (for sake of cleaness)

--- src/tests/pbc-test.cpp
+++ src/tests/pbc-test.cpp
@@ -12,6 +12,8 @@
 
 #include "bblock/system.h"
 
+//#define PRINT_XYZ_PBC_TEST
+
 namespace {
 
 static std::vector<bblock::System> systems;
@@ -58,12 +60,14 @@ int main(int argc, char** argv)
     std::vector<std::string> ats = systems[i].GetOriginalOrderSysAtNames();
     double e1b = systems[i].Get1B(false);
     double e2b = systems[i].Get2B(false);
+    double e3b = systems[i].Get3B(false);
 
     // Then PBC
     systems[i].SetPBC(true,box);
     std::vector<double> xyz_pbc = systems[i].GetOriginalOrderSysXyz();
     double e1b_pbc = systems[i].Get1B(false);
     double e2b_pbc = systems[i].Get2B(false);
+    double e3b_pbc = systems[i].Get3B(false);
 
     // Print output and differences
     std::cout << std::setprecision(6) << std::scientific
@@ -84,6 +88,16 @@ int main(int argc, char** argv)
               << std::setw(15) << std::right << e2b_pbc << std::setfill(' ')
               << std::setw(12) << std::right << " kcal/mol" << std::endl;          
 
+    std::cout << std::setprecision(6) << std::scientific
+              << "system["  << std::setfill('.')
+              << std::setw(10) << i
+              << std::setw(12) << "] e3b(nopbc)= " << std::setfill(' ')
+              << std::setw(15) << std::right <<  e3b << std::setfill(' ')
+              << std::setw(12) << " e3b(pbc)= " << std::setfill(' ')
+              << std::setw(15) << std::right << e3b_pbc << std::setfill(' ')
+              << std::setw(12) << std::right << " kcal/mol" << std::endl;
+
+#   ifdef PRINT_XYZ_PBC_TEST
     std::cout << "\nXYZ no PBC\n " << ats.size() << "\n 0.0 \n"; 
     for (size_t j = 0; j < ats.size(); j++) {
       std::cout << std::setprecision(6) << std::scientific
@@ -101,7 +115,8 @@ int main(int argc, char** argv)
                 << std::setw(15) << std::right << xyz_pbc[3*j + 1]
                 << std::setw(15) << std::right << xyz_pbc[3*j + 2] << std::endl;
     }
-    
+#   endif    
+
     std::cout << std::endl;
   }
   return 0;
