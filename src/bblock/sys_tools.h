e2198db89b2af870dc57d3fe8caf1122aa8b5060
Marc Riera <mrierari@ucsd.edu>
2018-09-24 17:58:56 -0700

    Started documentation for sys_tools.h

--- src/bblock/sys_tools.h
+++ src/bblock/sys_tools.h
@@ -15,12 +15,63 @@
 #include "potential/1b/ps.h"
 #include "tools/constants.h"
 
+/** 
+ * @file sys_tools.h 
+ * @brief Function definitions for system class tools
+ */
+
+/**
+ * @namespace systools
+ * @brief Namespace that contains the helper functions for the system
+ * class
+ *
+ * These functions are callable from anywhere, but are specifically designed
+ * to assist the system class member functions.
+ */
 namespace systools {
 
-// Function that given the monomers, modifies the ordered monomer list
-// in mon, and the original order in the vector original_order
-// taking sites into account, while original_order_realSites only takes
-// into account real sites
+/** 
+ * @brief Orders the monomers into the internal order, putting
+ * equivalent monomers one after the otehr one to maximize efficiency.
+ *
+ * This function will reorder the monomer list in a way that can maximize
+ * the efficiency in the parallel loops. It will put all the equivalent
+ * monomers together, one after the other one, from less frquent to 
+ * more frequent. 
+ * @param[in,out] mon List of monomers in the input order that will be
+ * cleared and replaced by the list of monomers in the internal order.
+ * @param[in] sites Vector of size_t with the number of sites of each
+ * monomer in the input order
+ * @param[in] nats Vector of size_t with the number of real sites of each
+ * monomer in the input order
+ * @param[out] original2current_order This vector will contain the relation
+ * between the input order and the interanl order. As an example, 
+ * @verbatim  original2current_order[4] @endverbatim 
+ * will contain the position in the internal order of the 5th monomer
+ * in the input order
+ * @param[out] original_order This vector of pairs contains the input order
+ * and the first index in the input order of a given monomer index. 
+ * In this case, the virtual sites are taken into account. As an example,
+ * @verbatim original_order[3].first @endverbatim
+ * will contain the index in the input order of the 4th monomer in the
+ * internal order, and
+ * @verbatim original_order[3].second @endverbatim 
+ * contains the first index of the 4th monomer in the internal order
+ * when it was in the input order
+ * @param[out] original_order_realSites This vector of pairs contains the input
+ * order and the first index in the input order of a given monomer index. 
+ * In this case, the virtual sites are not taken into account. As an example,
+ * @verbatim original_order_realSites[3].first @endverbatim
+ * will contain the index in the input order of the 4th monomer in the
+ * internal order, and
+ * @verbatim original_order_realSites[3].second @endverbatim 
+ * contains the first index of the 4th monomer in the internal order
+ * when it was in the input order with virtual sites not taken into account
+ * @return An ordered vector of pairs. Each pair has the monomer type id
+ * in the first place, and the number of that monomer type in the second one.
+ * The order inside this vector is from less number of monomers to more 
+ * number of monomers.
+ */
 std::vector<std::pair<std::string,size_t>> OrderMonomers
          (std::vector<std::string> &mon,
           std::vector<size_t> sites,
@@ -29,23 +80,96 @@ std::vector<std::pair<std::string,size_t>> OrderMonomers
           std::vector<std::pair<size_t,size_t> > &original_order,
           std::vector<std::pair<size_t,size_t> > &original_order_realSites);
 
-// Function that sets up initial charges, pols, polfacs, number of sites
-// number of atoms and first index. Returns the total number of sites
+/**
+ * @brief Function that fills the sites, atoms and first index of 
+ * the system given the list of monomer ids
+ *
+ * Given the monomers in an arbitrary order, this function creates the sites,
+ * atoms, and first index vectors for that order.
+ * @param[in] mon A vector of strings with the ids of the monomers of
+ * the system
+ * @param[out] sites Vector with same length as mon than contains the number
+ * of sites of the monomers in the same order as the mon vector
+ * @param[out] nat Vector with same length as mon than contains the number
+ * of atoms of the monomers in the same order as the mon vector 
+ * @param[out] fi_at Vector with same length as mon than contains the first
+ * index of the monomers in the same order as the mon vector
+ * @return Total number of sites
+ */
 size_t SetUpMonomers(std::vector<std::string> mon, std::vector<size_t> &sites,
                      std::vector<size_t> &nat, std::vector<size_t> &fi_at);
 
-// Makes sure that all the atoms of the monomers are together in space
+/**
+ * @brief Makes sure that the coordinates of all atoms of the same monomer
+ * are in the same region of the space
+ *
+ * This function is only useful when periodic boundary conditions are used.
+ * It will put all the atoms of the monomer, no matter where they are,
+ * close to the first atom of that monomer.
+ * @param[in,out] xyz Vector of doubles with all the monomer coordinates
+ * that will be replaced by the fixed monomer coordinates after applying
+ * mirror images of the atoms. It will take 
+ * @param[in] box Vector of 9 component with the 3 vectors of the box
+ * @param[in] nat Vector with the number of atoms of each monomer
+ * @param[in] first_index Vector with the first index of each monomer
+ */
 void FixMonomerCoordinates(std::vector<double> &xyz,
                            std::vector<double> box,
                            std::vector<size_t> nat,
                            std::vector<size_t> first_index);                           
 
-// Puts the two monomers of a dimer together for a given set of xyz
+/**
+ * @brief This function finds the monomer 2 mirror image that is closer to 
+ * the monomer 1 image.
+ *
+ * In periodic boundary conditions, it will look for the image of the monomer
+ * in xyz2 that is closer to the monomer in xyz1. This function assumes that
+ * all monomers in xyz1 are of the same type, and all monomers in xyz2 are
+ * also of the same type.
+ * Monomers in xyz1, xyz2, and xyz3 can be of a different type.
+ * The monomers in xyz1 and xyz2 have to be paired,
+ * meaning that monomer 1 in xyz1 and monomer 1 in xyz2 are the first dimer
+ * @param[in] box Vector of 9 component with the 3 vectors of the box
+ * @param[in] nat1 Number of atoms of monomer 1
+ * @param[in] nat2 Number of atoms of monomer 2
+ * @param[in] nd Number of dimers
+ * @param[in,out] xyz1 coordinates of all the monomers 1, one after
+ * the other one. At output, it will be untouched.
+ * @param[in,out] xyz2 coordinates of all the monomers 2, one after
+ * the other one. At output, it will be modified so the coordinates
+ * of monomer 2 are the closer image to monomer 1.
+ */
 void GetCloseDimerImage(std::vector<double> box,
                         size_t nat1, size_t nat2, size_t nd,
                         double * xyz1, double * xyz2);
 
-// Puts the three monomers of a trimer together for a given set of xyz
+/**
+ * @brief This function finds the monomers 2 and 3 mirror image that is 
+ * closer to the monomer 1 image.
+ *
+ * In periodic boundary conditions, it will look for the image of the monomer
+ * in xyz2 and xyz3 that is closer to the monomer in xyz1. 
+ * This function assumes that
+ * all monomers in xyz1 are of the same type, and all monomers in xyz2 are
+ * also of the same type, and all monomers in xyz3 are 
+ * also of the same type. 
+ * Monomers in xyz1, xyz2, and xyz3 can be of a different type.
+ * The monomers in xyz1, xyz2, and xyz3 have to be paired,
+ * meaning that monomer 1 in xyz1, monomer 1 in xyz2, and monomer 1
+ * in xyz3 are the first trimer
+ * @param[in] box Vector of 9 component with the 3 vectors of the box
+ * @param[in] nat1 Number of atoms of monomer 1
+ * @param[in] nat2 Number of atoms of monomer 2
+ * @param[in] nat3 Number of atoms of monomer 3
+ * @param[in] nt Number of dimers
+ * @param[in,out] xyz1 coordinates of all the monomers 1, one after
+ * the other one. At output, it will be untouched.
+ * @param[in,out] xyz2 coordinates of all the monomers 2, one after
+ * the other one. At output, it will be modified so the coordinates
+ * @param[in,out] xyz3 coordinates of all the monomers 3, one after
+ * the other one. At output, it will be modified so the coordinates
+ * of monomer 3 are the closer image to monomer 1.
+ */
 void GetCloseTrimerImage(std::vector<double> box,
                         size_t nat1, size_t nat2, size_t nat3, size_t nt,
                         double * xyz1, double * xyz2, double * xyz3);
