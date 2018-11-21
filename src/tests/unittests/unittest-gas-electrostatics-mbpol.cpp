9e7ad5dac4d018a30b183275911b803c2ede476a
Marc Riera <mrierari@ucsd.edu>
2018-11-21 08:19:27 -0800

    Added sections to identify different tests in unittest-gas-electrostatics-mbpol.cpp

--- src/tests/unittests/unittest-gas-electrostatics-mbpol.cpp
+++ src/tests/unittests/unittest-gas-electrostatics-mbpol.cpp
@@ -13,21 +13,23 @@ constexpr double TOL = 1E-6;
 TEST_CASE("test the electrostatics class for mbpol (gas phase).") {
     // TIP3P test
     SETUP_WATERBOX_3_MBPOL
-    SECTION("Total electrostatic energy and forces and individual terms") {
-        elec::Electrostatics elec;
-        elec.Initialize(charges, chg_grad, polfac, pol, coords, monomer_names, sites, first_ind, mon_type_count, true,
-                        1E-16, 100, "iter", std::vector<double>{});
-        std::vector<double> forces(3 * n_atoms);
-        double energy = elec.GetElectrostatics(forces);
-        double perm_elec = elec.GetPermanentElectrostaticEnergy();
-        double ind_elec = elec.GetInducedElectrostaticEnergy();
+    elec::Electrostatics elec;
+    elec.Initialize(charges, chg_grad, polfac, pol, coords, monomer_names, sites, first_ind, mon_type_count, true,
+                    1E-16, 100, "iter", std::vector<double>{});
+    std::vector<double> forces(3 * n_atoms);
+    double energy = elec.GetElectrostatics(forces);
+    double perm_elec = elec.GetPermanentElectrostaticEnergy();
+    double ind_elec = elec.GetInducedElectrostaticEnergy();
 
+    SECTION("Permanent electrostatic energy.") { REQUIRE(perm_elec == Approx(-1.8567131645e+01).margin(TOL)); }
+
+    SECTION("Induced electrostatic energy.") { REQUIRE(ind_elec == Approx(-6.4991884968e+00).margin(TOL)); }
+
+    SECTION("Total electrostatic energy.") {
         REQUIRE(energy == Approx(-2.5066320142e+01).margin(TOL));
-        REQUIRE(perm_elec == Approx(-1.8567131645e+01).margin(TOL));
-        REQUIRE(ind_elec == Approx(-6.4991884968e+00).margin(TOL));
 
-        for (size_t i = 0; i < grad.size(); i++)
-            REQUIRE(forces[i] == Approx(grad[i]).margin(TOL));
-
+        SECTION("Forces of the total electrostatic energy") {
+            for (size_t i = 0; i < grad.size(); i++) REQUIRE(forces[i] == Approx(grad[i]).margin(TOL));
+        }
     }
 }
