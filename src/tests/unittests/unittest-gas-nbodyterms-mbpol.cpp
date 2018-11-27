f67f72a97e13783253a7d2c8489d502a47497abb
Marc Riera <mrierari@ucsd.edu>
2018-11-27 09:30:16 -0800

    Formatted unittests with clang

--- src/tests/unittests/unittest-gas-nbodyterms-mbpol.cpp
+++ src/tests/unittests/unittest-gas-nbodyterms-mbpol.cpp
@@ -13,14 +13,15 @@
 
 constexpr double TOL = 1E-6;
 
-void GetDimers(const std::vector<double> xyz, const size_t nmon, std::vector<double> &xyz1, std::vector<double> &xyz2, size_t &ndimers) {
+void GetDimers(const std::vector<double> xyz, const size_t nmon, std::vector<double> &xyz1, std::vector<double> &xyz2,
+               size_t &ndimers) {
     ndimers = 0;
     xyz1.clear();
     xyz2.clear();
     for (size_t i = 0; i < nmon - 1; i++) {
-        size_t offset_i = 9*i;
-        for (size_t j = i+1; j < nmon; j++) {
-            size_t offset_j = 9*j;
+        size_t offset_i = 9 * i;
+        for (size_t j = i + 1; j < nmon; j++) {
+            size_t offset_j = 9 * j;
             for (size_t k = 0; k < 9; k++) {
                 xyz1.push_back(xyz[offset_i + k]);
                 xyz2.push_back(xyz[offset_j + k]);
@@ -30,7 +31,6 @@ void GetDimers(const std::vector<double> xyz, const size_t nmon, std::vector<dou
     }
 }
 
-
 TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
     // MB-pol test
     SETUP_WATERBOX_3_MBPOL
@@ -39,26 +39,22 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
     std::string monomer_name = "h2o";
 
     // Obtain coordinates without virtual sites
-    std::vector<double> real_xyz(3*n_atoms,0.0);
+    std::vector<double> real_xyz(3 * n_atoms, 0.0);
     size_t pos = 0;
     for (size_t i = 0; i < nmon; i++) {
-        size_t shift = first_ind[i]*3;
+        size_t shift = first_ind[i] * 3;
         std::copy(coords.begin() + shift, coords.begin() + shift + 9, real_xyz.begin() + pos);
         pos += 9;
     }
 
     SECTION("One body (polynomials)") {
-        std::vector<double> grad(3*n_atoms,0.0);
-        double energy_1b_nograd = e1b::get_1b_energy(monomer_name,nmon,real_xyz,good);
-        double energy_1b_grad = e1b::get_1b_energy(monomer_name,nmon,real_xyz,grad,good);
+        std::vector<double> grad(3 * n_atoms, 0.0);
+        double energy_1b_nograd = e1b::get_1b_energy(monomer_name, nmon, real_xyz, good);
+        double energy_1b_grad = e1b::get_1b_energy(monomer_name, nmon, real_xyz, grad, good);
 
-        SECTION("Energy without gradients.") {
-             REQUIRE(energy_1b_nograd == Approx(3.2265445802e-01).margin(TOL));
-        }
+        SECTION("Energy without gradients.") { REQUIRE(energy_1b_nograd == Approx(3.2265445802e-01).margin(TOL)); }
 
-        SECTION("Energy with gradients") {
-             REQUIRE(energy_1b_grad == Approx(3.2265445802e-01).margin(TOL));
-        }
+        SECTION("Energy with gradients") { REQUIRE(energy_1b_grad == Approx(3.2265445802e-01).margin(TOL)); }
 
         SECTION("Numerical gradients vs analitical gradients") {
             size_t atomOffset = 0;
@@ -66,9 +62,9 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
             for (size_t molecule = 0; molecule < nmon; ++molecule) {
                 for (size_t degreeOfFreedom = atomOffset; degreeOfFreedom < atomOffset + 9; ++degreeOfFreedom) {
                     real_xyz[degreeOfFreedom] += stepSize;
-                    double plusEnergy = e1b::get_1b_energy(monomer_name,nmon,real_xyz,good);
+                    double plusEnergy = e1b::get_1b_energy(monomer_name, nmon, real_xyz, good);
                     real_xyz[degreeOfFreedom] -= 2 * stepSize;
-                    double minusEnergy = e1b::get_1b_energy(monomer_name,nmon,real_xyz,good);
+                    double minusEnergy = e1b::get_1b_energy(monomer_name, nmon, real_xyz, good);
                     real_xyz[degreeOfFreedom] += stepSize;
                     double finiteDifferenceForce = (plusEnergy - minusEnergy) / (2 * stepSize);
                     double error = grad[degreeOfFreedom] - finiteDifferenceForce;
@@ -78,27 +74,26 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
         }
     }
 
-
     SECTION("Two body (polynomials)") {
         // Obtain the dimers in the right format
-        // Need 2 xyz vectors paired. 
+        // Need 2 xyz vectors paired.
         std::vector<double> xyz1;
         std::vector<double> xyz2;
-        std::vector<double> grad(3*n_atoms);
+        std::vector<double> grad(3 * n_atoms);
         size_t ndimers = 0;
-        GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
+        GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
         // Declare gradient vectors
-        std::vector<double> grad1(xyz1.size(),0.0);
-        std::vector<double> grad2(xyz2.size(),0.0);
+        std::vector<double> grad1(xyz1.size(), 0.0);
+        std::vector<double> grad2(xyz2.size(), 0.0);
 
-        double energy_2b_nograd = e2b::get_2b_energy(monomer_name,monomer_name,ndimers,xyz1,xyz2);
-        double energy_2b_grad = e2b::get_2b_energy(monomer_name,monomer_name,ndimers,xyz1,xyz2,grad1,grad2);
+        double energy_2b_nograd = e2b::get_2b_energy(monomer_name, monomer_name, ndimers, xyz1, xyz2);
+        double energy_2b_grad = e2b::get_2b_energy(monomer_name, monomer_name, ndimers, xyz1, xyz2, grad1, grad2);
         // Add the gradients properly
         size_t offset_dimer = 0;
         for (size_t i = 0; i < nmon - 1; i++) {
-            size_t offset_i = 9*i;
-            for (size_t j = i+1; j < nmon; j++) {
-                size_t offset_j = 9*j;
+            size_t offset_i = 9 * i;
+            for (size_t j = i + 1; j < nmon; j++) {
+                size_t offset_j = 9 * j;
                 for (size_t k = 0; k < 9; k++) {
                     grad[offset_i + k] += grad1[offset_dimer + k];
                     grad[offset_j + k] += grad2[offset_dimer + k];
@@ -106,14 +101,10 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
                 offset_dimer += 9;
             }
         }
-        
-        SECTION("Energy without gradients.") {
-             REQUIRE(energy_2b_nograd == Approx(1.7011845631e+01).margin(TOL));
-        }
 
-        SECTION("Energy with gradients") {
-             REQUIRE(energy_2b_grad == Approx(1.7011845631e+01).margin(TOL));
-        }
+        SECTION("Energy without gradients.") { REQUIRE(energy_2b_nograd == Approx(1.7011845631e+01).margin(TOL)); }
+
+        SECTION("Energy with gradients") { REQUIRE(energy_2b_grad == Approx(1.7011845631e+01).margin(TOL)); }
 
         SECTION("Numerical gradients vs analitical gradients") {
             size_t atomOffset = 0;
@@ -121,11 +112,11 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
             for (size_t molecule = 0; molecule < nmon; ++molecule) {
                 for (size_t degreeOfFreedom = atomOffset; degreeOfFreedom < atomOffset + 9; ++degreeOfFreedom) {
                     real_xyz[degreeOfFreedom] += stepSize;
-                    GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
-                    double plusEnergy = e2b::get_2b_energy(monomer_name,monomer_name,ndimers,xyz1,xyz2);
+                    GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
+                    double plusEnergy = e2b::get_2b_energy(monomer_name, monomer_name, ndimers, xyz1, xyz2);
                     real_xyz[degreeOfFreedom] -= 2 * stepSize;
-                    GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
-                    double minusEnergy = e2b::get_2b_energy(monomer_name,monomer_name,ndimers,xyz1,xyz2);
+                    GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
+                    double minusEnergy = e2b::get_2b_energy(monomer_name, monomer_name, ndimers, xyz1, xyz2);
                     real_xyz[degreeOfFreedom] += stepSize;
                     double finiteDifferenceForce = (plusEnergy - minusEnergy) / (2 * stepSize);
                     double error = grad[degreeOfFreedom] - finiteDifferenceForce;
@@ -138,24 +129,26 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
     SECTION("Two body (dispersion)") {
         std::vector<double> xyz1;
         std::vector<double> xyz2;
-        std::vector<double> grad(3*n_atoms);
+        std::vector<double> grad(3 * n_atoms);
 
         size_t ndimers = 0;
-        GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
+        GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
 
         // Declare gradient vectors
-        std::vector<double> grad1(xyz1.size(),0.0);
-        std::vector<double> grad2(xyz2.size(),0.0);
+        std::vector<double> grad1(xyz1.size(), 0.0);
+        std::vector<double> grad2(xyz2.size(), 0.0);
 
-        double energy_2b_nograd = disp::GetDispersion(monomer_name,monomer_name,ndimers,false,xyz1,xyz2,grad1,grad2, 100.0,false);
-        double energy_2b_grad = disp::GetDispersion(monomer_name,monomer_name,ndimers,true,xyz1,xyz2,grad1,grad2, 100.0,false);
+        double energy_2b_nograd =
+            disp::GetDispersion(monomer_name, monomer_name, ndimers, false, xyz1, xyz2, grad1, grad2, 100.0, false);
+        double energy_2b_grad =
+            disp::GetDispersion(monomer_name, monomer_name, ndimers, true, xyz1, xyz2, grad1, grad2, 100.0, false);
 
         // Add the gradients properly
         size_t offset_dimer = 0;
         for (size_t i = 0; i < nmon - 1; i++) {
-            size_t offset_i = 9*i;
-            for (size_t j = i+1; j < nmon; j++) {
-                size_t offset_j = 9*j;
+            size_t offset_i = 9 * i;
+            for (size_t j = i + 1; j < nmon; j++) {
+                size_t offset_j = 9 * j;
                 for (size_t k = 0; k < 9; k++) {
                     grad[offset_i + k] += grad1[offset_dimer + k];
                     grad[offset_j + k] += grad2[offset_dimer + k];
@@ -164,13 +157,9 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
             }
         }
 
-        SECTION("Energy without gradients.") {
-             REQUIRE(energy_2b_nograd == Approx(-8.1876933060e+00).margin(TOL));
-        }
+        SECTION("Energy without gradients.") { REQUIRE(energy_2b_nograd == Approx(-8.1876933060e+00).margin(TOL)); }
 
-        SECTION("Energy with gradients") {
-             REQUIRE(energy_2b_grad == Approx(-8.1876933060e+00).margin(TOL));
-        }
+        SECTION("Energy with gradients") { REQUIRE(energy_2b_grad == Approx(-8.1876933060e+00).margin(TOL)); }
 
         SECTION("Numerical gradients vs analitical gradients") {
             size_t atomOffset = 0;
@@ -178,11 +167,13 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
             for (size_t molecule = 0; molecule < nmon; ++molecule) {
                 for (size_t degreeOfFreedom = atomOffset; degreeOfFreedom < atomOffset + 9; ++degreeOfFreedom) {
                     real_xyz[degreeOfFreedom] += stepSize;
-                    GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
-                    double plusEnergy = disp::GetDispersion(monomer_name,monomer_name,ndimers,false,xyz1,xyz2,grad1,grad2, 100.0,false);
+                    GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
+                    double plusEnergy = disp::GetDispersion(monomer_name, monomer_name, ndimers, false, xyz1, xyz2,
+                                                            grad1, grad2, 100.0, false);
                     real_xyz[degreeOfFreedom] -= 2 * stepSize;
-                    GetDimers(real_xyz,nmon,xyz1,xyz2,ndimers);
-                    double minusEnergy = disp::GetDispersion(monomer_name,monomer_name,ndimers,false,xyz1,xyz2,grad1,grad2, 100.0,false);
+                    GetDimers(real_xyz, nmon, xyz1, xyz2, ndimers);
+                    double minusEnergy = disp::GetDispersion(monomer_name, monomer_name, ndimers, false, xyz1, xyz2,
+                                                             grad1, grad2, 100.0, false);
                     real_xyz[degreeOfFreedom] += stepSize;
                     double finiteDifferenceForce = (plusEnergy - minusEnergy) / (2 * stepSize);
                     double error = grad[degreeOfFreedom] - finiteDifferenceForce;
@@ -205,22 +196,19 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
         std::copy(real_xyz.begin() + 9, real_xyz.begin() + 18, xyz2.begin());
         std::copy(real_xyz.begin() + 18, real_xyz.begin() + 27, xyz3.begin());
 
-        std::vector<double> grad(3*n_atoms);
-        double energy_3b_nograd = e3b::get_3b_energy(monomer_name,monomer_name,monomer_name,1,xyz1,xyz2,xyz3);
-        double energy_3b_grad = e3b::get_3b_energy(monomer_name,monomer_name,monomer_name,1,xyz1,xyz2,xyz3,grad1,grad2,grad3);
-        
+        std::vector<double> grad(3 * n_atoms);
+        double energy_3b_nograd = e3b::get_3b_energy(monomer_name, monomer_name, monomer_name, 1, xyz1, xyz2, xyz3);
+        double energy_3b_grad =
+            e3b::get_3b_energy(monomer_name, monomer_name, monomer_name, 1, xyz1, xyz2, xyz3, grad1, grad2, grad3);
+
         // Add the gradients properly
-        std::copy(grad1.begin(),grad1.end(),grad.begin());
-        std::copy(grad2.begin(),grad2.end(),grad.begin() + 9);
-        std::copy(grad3.begin(),grad3.end(),grad.begin() + 18);
+        std::copy(grad1.begin(), grad1.end(), grad.begin());
+        std::copy(grad2.begin(), grad2.end(), grad.begin() + 9);
+        std::copy(grad3.begin(), grad3.end(), grad.begin() + 18);
 
-        SECTION("Energy without gradients.") {
-             REQUIRE(energy_3b_nograd == Approx(2.2777705108e-01).margin(TOL));
-        }
+        SECTION("Energy without gradients.") { REQUIRE(energy_3b_nograd == Approx(2.2777705108e-01).margin(TOL)); }
 
-        SECTION("Energy with gradients") {
-             REQUIRE(energy_3b_grad == Approx(2.2777705108e-01).margin(TOL));
-        }
+        SECTION("Energy with gradients") { REQUIRE(energy_3b_grad == Approx(2.2777705108e-01).margin(TOL)); }
 
         SECTION("Numerical gradients vs analitical gradients") {
             size_t atomOffset = 0;
@@ -231,12 +219,14 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
                     std::copy(real_xyz.begin(), real_xyz.begin() + 9, xyz1.begin());
                     std::copy(real_xyz.begin() + 9, real_xyz.begin() + 18, xyz2.begin());
                     std::copy(real_xyz.begin() + 18, real_xyz.begin() + 27, xyz3.begin());
-                    double plusEnergy = e3b::get_3b_energy(monomer_name,monomer_name,monomer_name,1,xyz1,xyz2,xyz3);
+                    double plusEnergy =
+                        e3b::get_3b_energy(monomer_name, monomer_name, monomer_name, 1, xyz1, xyz2, xyz3);
                     real_xyz[degreeOfFreedom] -= 2 * stepSize;
                     std::copy(real_xyz.begin(), real_xyz.begin() + 9, xyz1.begin());
                     std::copy(real_xyz.begin() + 9, real_xyz.begin() + 18, xyz2.begin());
                     std::copy(real_xyz.begin() + 18, real_xyz.begin() + 27, xyz3.begin());
-                    double minusEnergy = e3b::get_3b_energy(monomer_name,monomer_name,monomer_name,1,xyz1,xyz2,xyz3);
+                    double minusEnergy =
+                        e3b::get_3b_energy(monomer_name, monomer_name, monomer_name, 1, xyz1, xyz2, xyz3);
                     real_xyz[degreeOfFreedom] += stepSize;
                     double finiteDifferenceForce = (plusEnergy - minusEnergy) / (2 * stepSize);
                     double error = grad[degreeOfFreedom] - finiteDifferenceForce;
@@ -246,5 +236,3 @@ TEST_CASE("Test the n-body terms for mbpol (gas phase).") {
         }
     }
 }
-
-
--- src/tests/unittests/unittest-pme-coulombonly.cpp
+++ src/tests/unittests/unittest-pme-coulombonly.cpp
@@ -35,7 +35,6 @@ TEST_CASE("test the electrostatics class for coulomb terms (PME).") {
     double energy3 = elec.GetElectrostatics(forces3);
     REQUIRE(energy3 == Approx(ref_energy).margin(TOL));
 
-
     // alpha = 0.4
     elec.Initialize(charges, chg_grad, polfac, pol, coords, monomer_names, sites, first_ind, mon_type_count, true,
                     1E-16, 100, "iter", box_vectors);
@@ -46,7 +45,7 @@ TEST_CASE("test the electrostatics class for coulomb terms (PME).") {
     std::vector<double> forces4(3 * n_atoms);
     double energy4 = elec.GetElectrostatics(forces4);
     REQUIRE(energy4 == Approx(energy3).margin(TOL));
-    for(int n = 0; n < 3*n_atoms; ++n) REQUIRE(forces3[n] == Approx(forces4[n]).margin(TOL));
+    for (int n = 0; n < 3 * n_atoms; ++n) REQUIRE(forces3[n] == Approx(forces4[n]).margin(TOL));
 
     // alpha = 0.5
     elec.Initialize(charges, chg_grad, polfac, pol, coords, monomer_names, sites, first_ind, mon_type_count, true,
@@ -58,5 +57,5 @@ TEST_CASE("test the electrostatics class for coulomb terms (PME).") {
     std::vector<double> forces5(3 * n_atoms);
     double energy5 = elec.GetElectrostatics(forces5);
     REQUIRE(energy5 == Approx(energy4).margin(TOL));
-    for(int n = 0; n < 3*n_atoms; ++n) REQUIRE(forces4[n] == Approx(forces5[n]).margin(TOL));
+    for (int n = 0; n < 3 * n_atoms; ++n) REQUIRE(forces4[n] == Approx(forces5[n]).margin(TOL));
 }
