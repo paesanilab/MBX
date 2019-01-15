2c6707f3fb445bcb4679fb5616b10ca5ae940e9a
Marc Riera <mrierari@ucsd.edu>
2019-01-15 09:36:13 -0800

    Commented inversion matrix routine in electrostatics. It is now in math tools.

--- src/potential/electrostatics/electrostatics.cpp
+++ src/potential/electrostatics/electrostatics.cpp
@@ -17,23 +17,23 @@ namespace elec {
 
 const double SQRTPI = sqrt(M_PI);
 
-std::vector<double> InvertUnitCell(const std::vector<double> &box) {
-    double determinant = box[0] * (box[4] * box[8] - box[7] * box[5]) - box[1] * (box[3] * box[8] - box[5] * box[6]) +
-                         box[2] * (box[3] * box[7] - box[4] * box[6]);
-
-    double determinant_inverse = 1 / determinant;
-    std::vector<double> box_inverse(9);
-    box_inverse[0] = (box[4] * box[8] - box[7] * box[5]) * determinant_inverse;
-    box_inverse[1] = (box[2] * box[7] - box[1] * box[8]) * determinant_inverse;
-    box_inverse[2] = (box[1] * box[5] - box[2] * box[4]) * determinant_inverse;
-    box_inverse[3] = (box[5] * box[6] - box[3] * box[8]) * determinant_inverse;
-    box_inverse[4] = (box[0] * box[8] - box[2] * box[6]) * determinant_inverse;
-    box_inverse[5] = (box[3] * box[2] - box[0] * box[5]) * determinant_inverse;
-    box_inverse[6] = (box[3] * box[7] - box[6] * box[4]) * determinant_inverse;
-    box_inverse[7] = (box[6] * box[1] - box[0] * box[7]) * determinant_inverse;
-    box_inverse[8] = (box[0] * box[4] - box[3] * box[1]) * determinant_inverse;
-    return box_inverse;
-}
+//std::vector<double> InvertUnitCell(const std::vector<double> &box) {
+//    double determinant = box[0] * (box[4] * box[8] - box[7] * box[5]) - box[1] * (box[3] * box[8] - box[5] * box[6]) +
+//                         box[2] * (box[3] * box[7] - box[4] * box[6]);
+//
+//    double determinant_inverse = 1 / determinant;
+//    std::vector<double> box_inverse(9);
+//    box_inverse[0] = (box[4] * box[8] - box[7] * box[5]) * determinant_inverse;
+//    box_inverse[1] = (box[2] * box[7] - box[1] * box[8]) * determinant_inverse;
+//    box_inverse[2] = (box[1] * box[5] - box[2] * box[4]) * determinant_inverse;
+//    box_inverse[3] = (box[5] * box[6] - box[3] * box[8]) * determinant_inverse;
+//    box_inverse[4] = (box[0] * box[8] - box[2] * box[6]) * determinant_inverse;
+//    box_inverse[5] = (box[3] * box[2] - box[0] * box[5]) * determinant_inverse;
+//    box_inverse[6] = (box[3] * box[7] - box[6] * box[4]) * determinant_inverse;
+//    box_inverse[7] = (box[6] * box[1] - box[0] * box[7]) * determinant_inverse;
+//    box_inverse[8] = (box[0] * box[4] - box[3] * box[1]) * determinant_inverse;
+//    return box_inverse;
+//}
 
 void Electrostatics::SetCutoff(double cutoff) { cutoff_ = cutoff; }
 
