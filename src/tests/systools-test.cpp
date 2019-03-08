6ec74b814292f5112935a57fbdf3bbfd5db6cfcf
Marc Riera <mrierari@ucsd.edu>
2019-03-08 09:33:04 -0800

    Added license tot est mains

--- src/tests/systools-test.cpp
+++ src/tests/systools-test.cpp
@@ -1,3 +1,37 @@
+/******************************************************************************
+Copyright 2019 The Regents of the University of California.
+All Rights Reserved.
+
+Permission to copy, modify and distribute any part of this Software for
+educational, research and non-profit purposes, without fee, and without
+a written agreement is hereby granted, provided that the above copyright
+notice, this paragraph and the following three paragraphs appear in all
+copies.
+
+Those desiring to incorporate this Software into commercial products or
+use for commercial purposes should contact the:
+Office of Innovation & Commercialization
+University of California, San Diego
+9500 Gilman Drive, Mail Code 0910
+La Jolla, CA 92093-0910
+Ph: (858) 534-5815
+FAX: (858) 534-7345
+E-MAIL: invent@ucsd.edu
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE, EVEN IF THE UNIVERSITY
+OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS. THE UNIVERSITY OF CALIFORNIA MAKES NO
+REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER IMPLIED OR
+EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF THE
+SOFTWARE WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER RIGHTS.
+******************************************************************************/
+
 #include <cmath>
 #include <cassert>
 
@@ -71,7 +105,9 @@ int main(int argc, char **argv) {
         -2.52638e-02, -1.13690e+00, 2.81426e+00,  -1.80176e-01};
 
     // Setting expected charges
-    std::vector<double> chg_expected = {0.0000000000e+00, 5.7769308798e-01, 5.7615998121e-01, -1.1538530692e+00, 0.0000000000e+00, 5.7663564934e-01, 5.7823701694e-01, -1.1548726663e+00, 0.0000000000e+00, 5.7859034043e-01, 5.7709162608e-01, -1.1556819665e+00};
+    std::vector<double> chg_expected = {0.0000000000e+00, 5.7769308798e-01, 5.7615998121e-01, -1.1538530692e+00,
+                                        0.0000000000e+00, 5.7663564934e-01, 5.7823701694e-01, -1.1548726663e+00,
+                                        0.0000000000e+00, 5.7859034043e-01, 5.7709162608e-01, -1.1556819665e+00};
 
     // Setting expected polarizabilities
     std::vector<double> pol_expected = {1.31000e+00, 2.94000e-01, 2.94000e-01, 0.00000e+00, 1.31000e+00, 2.94000e-01,
