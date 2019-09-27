61ce65317279039faf98d597a4ab5eb4a4900602
Marc Riera <mrierari@ucsd.edu>
2019-09-27 09:13:57 -0700

    Cleaned potential/1b

--- src/potential/1b/energy1b.cpp
+++ src/potential/1b/energy1b.cpp
@@ -44,12 +44,6 @@ double get_1b_energy(std::string mon1, size_t nm, std::vector<double> xyz1, bool
 
         // =====>> BEGIN SECTION 1B_NO_GRADIENT <<=====
         // =====>> PASTE YOUR CODE BELOW <<=====
-    } else if (mon1 == "pf6-") {
-        x1b_A1B6_deg5::x1b_A1B6_v1x pot(mon1);
-        energies = pot.eval(xyz1.data(), nm);
-    } else if (mon1 == "nh4+") {
-        x1b_A1B4_deg5::x1b_A1B4_v1x pot(mon1);
-        energies = pot.eval(xyz1.data(), nm);
     } else if (mon1 == "ch4") {
         x1b_A1B4_deg5_exp0::x1b_A1B4_v1x pot(mon1);
         energies = pot.eval(xyz1.data(), nm);
@@ -80,14 +74,8 @@ double get_1b_energy(std::string mon1, size_t nm, std::vector<double> xyz1, std:
 
         // =====>> BEGIN SECTION 1B_GRADIENT <<=====
         // ====>> PASTE YOUR CODE BELOW <<====
-    } else if (mon1 == "pf6-") {
-        x1b_A1B6_deg5::x1b_A1B6_v1x pot(mon1);
-        energies = pot.eval(xyz1.data(), grad1.data(), nm);
     } else if (mon1 == "ch4") {
         x1b_A1B4_deg5_exp0::x1b_A1B4_v1x pot(mon1);
-        energies = pot.eval(xyz1.data(), grad1.data(), nm);
-    } else if (mon1 == "nh4+") {
-        x1b_A1B4_deg5::x1b_A1B4_v1x pot(mon1);
         energies = pot.eval(xyz1.data(), grad1.data(), nm);
     } else if (mon1 == "co2") {
         x1b_A1B2_deg4::x1b_A1B2_v1x pot(mon1);
