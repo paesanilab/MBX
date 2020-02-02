b3c1b38794125ec2969384d2fed9a6906ad92de4
Marc Riera <mrierari@ucsd.edu>
2020-02-01 17:14:34 -0800

    Updated compilation scripts for tscc to use intel

--- scripts/supercomputer/tscc/compilation/compile_tscc.sh
+++ scripts/supercomputer/tscc/compilation/compile_tscc.sh
@@ -4,21 +4,27 @@ if [ $# -ne 1 ]; then
   exit
 fi
 
+export MBX_HOME=$HOME/codes/MBX
+
 export MODULEPATH=/projects/builder-group/jpg/modulefiles/applications:$MODULEPATH
 module load cmake/3.13.4
 
-module load intel gsl openmpi_ib fftw
+module load intel/2018.1.163 gsl openmpi_ib fftw
+module load gnu
+
 export PATH=$FFTWHOME/lib:$FFTWHOME/include:$PATH
 
-if [ "$1" == "gnu" ]; then
-  rm -rf build install
-  cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OPENMP=True -DCMAKE_CXX_FLAGS=" -fPIC -O2 -Wall -L$FFTWHOME/lib -I$FFTWHOME/include" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -H. -Bbuild
-  cd build
-  make -j 2 CXX=g++ CC=gcc
-  make install
-  cd ../
+rm -rf build install
+cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OPENMP=True -DCMAKE_CXX_FLAGS=" -fPIC -O2 -Wall -L$FFTWHOME/lib -I$FFTWHOME/include" -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc -H. -Bbuild
+cd build
+make -j 2 CXX=icpc CC=icc
+make install
+cd ../
 
-elif [ "$1" == "intel" ]; then
-  echo "Sorry, this is not available here yet..."
+# Compile the driver
+cd plugins/i-pi/src/main
+cp Makefile_tscc Makefile
+make
+cd -
 
-fi
+
