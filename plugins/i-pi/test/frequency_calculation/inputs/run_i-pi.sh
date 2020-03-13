66f34340a4691d20d87f63ed0c97fbe73b6cd3a8
Marc Riera <mrierari@ucsd.edu>
2020-03-13 11:28:58 -0700

    Updated run ipi in freq example

--- plugins/i-pi/test/frequency_calculation/inputs/run_i-pi.sh
+++ plugins/i-pi/test/frequency_calculation/inputs/run_i-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $HOME/codes/i-pi/
+cd $IPI_HOME
 source env.sh
 cd -
 
@@ -11,10 +11,10 @@ sleep 10
 
 export OMP_NUM_THREADS=4
 
-/home/mrierari/codes/MBX/plugins/i-pi/bin/driver config.nrg mbx.json
+$MBX_HOME/plugins/i-pi/bin/driver config.nrg mbx.json
 
 id=`cat ipi.id`
 kill $id
 
 # COnvert to cm-1
-convert_vib_eigvals_au_to_cm-1.py simulation.phonons.eigval > frequencies.dat
+python3 convert_vib_eigvals_au_to_cm-1.py simulation.phonons.eigval > frequencies.dat
