036b0fb7c8d81fe1648de64f4722ef4ab17d91f5
Marc Riera <mrierari@ucsd.edu>
2020-04-13 18:46:01 -0700

    Fixed another bug for general boxes. Now seems to work, giving same results as for DLPOLY for Ice II.

--- external/kdtree/kdtree_utils.h
+++ external/kdtree/kdtree_utils.h
@@ -66,37 +66,43 @@ struct PointCloud {
         const T d0 = p1[0] - pts[idx_p2].x;
         const T d1 = p1[1] - pts[idx_p2].y;
         const T d2 = p1[2] - pts[idx_p2].z;
-        if (!PBC) {
-            T d = d0 * d0 + d1 * d1 + d2 * d2;
-            return d;
-        } else {
-            T x_rec = pbcbox_inv[0] * d0 
-                    + pbcbox_inv[3] * d1 
-                    + pbcbox_inv[6] * d2;
-            T y_rec = pbcbox_inv[1] * d0 
-                    + pbcbox_inv[4] * d1 
-                    + pbcbox_inv[7] * d2;
-            T z_rec = pbcbox_inv[2] * d0 
-                    + pbcbox_inv[5] * d1 
-                    + pbcbox_inv[8] * d2;     
+        T d = d0 * d0 + d1 * d1 + d2 * d2;
+        //std::cout << "Point " << p1[0] << "," << p1[1] << "," << p1[2] 
+        //          << " ; idx2= " << idx_p2 
+        //          << "  Point2 " << pts[idx_p2].x << "," << pts[idx_p2].y << "," << pts[idx_p2].z
+        //          << ", d = " << d << std::endl;
+        return d;
+        //if (!PBC) {
+        //    T d = d0 * d0 + d1 * d1 + d2 * d2;
+        //    return d;
+        //} else {
+        //    T x_rec = pbcbox_inv[0] * d0 
+        //            + pbcbox_inv[3] * d1 
+        //            + pbcbox_inv[6] * d2;
+        //    T y_rec = pbcbox_inv[1] * d0 
+        //            + pbcbox_inv[4] * d1 
+        //            + pbcbox_inv[7] * d2;
+        //    T z_rec = pbcbox_inv[2] * d0 
+        //            + pbcbox_inv[5] * d1 
+        //            + pbcbox_inv[8] * d2;     
 
-            x_rec -= std::floor(x_rec + 0.5);
-            y_rec -= std::floor(y_rec + 0.5);
-            z_rec -= std::floor(z_rec + 0.5);
-            
-            T x = pbcbox[0] * x_rec
-                + pbcbox[3] * y_rec
-                + pbcbox[6] * z_rec;
-            T y = pbcbox[1] * x_rec
-                + pbcbox[4] * y_rec
-                + pbcbox[7] * z_rec;
-            T z = pbcbox[2] * x_rec
-                + pbcbox[5] * y_rec
-                + pbcbox[8] * z_rec;
+        //    x_rec -= std::floor(x_rec + 0.5);
+        //    y_rec -= std::floor(y_rec + 0.5);
+        //    z_rec -= std::floor(z_rec + 0.5);
+        //    
+        //    T x = pbcbox[0] * x_rec
+        //        + pbcbox[3] * y_rec
+        //        + pbcbox[6] * z_rec;
+        //    T y = pbcbox[1] * x_rec
+        //        + pbcbox[4] * y_rec
+        //        + pbcbox[7] * z_rec;
+        //    T z = pbcbox[2] * x_rec
+        //        + pbcbox[5] * y_rec
+        //        + pbcbox[8] * z_rec;
 
-            T d = x*x + y*y + z*z;
-            return d;
-        }
+        //    T d = x*x + y*y + z*z;
+        //    return d;
+        //}
     }
 
     // Returns the dim'th component of the idx'th point in the class:
@@ -168,40 +174,40 @@ PointCloud<T> XyzToCloud(std::vector<T> xyz, bool use_pbc, std::vector<T> box, s
 
     size_t np = xyz.size() / 3;
     size_t size = np;
-    //if (use_pbc) size *= 27;
+    if (use_pbc) size *= 27;
 
     ptc.pts.resize(size);
 
-    //std::vector<int> indxs = {0, -1, 1};
+    std::vector<int> indxs = {0, -1, 1};
 
     for (size_t i = 0; i < np; i++) {
         size_t i3 = 3 * i;
         ptc.pts[i].x = xyz[i3];
         ptc.pts[i].y = xyz[i3 + 1];
         ptc.pts[i].z = xyz[i3 + 2];
-        //if (use_pbc) {
-        //    for (size_t m2 = 0; m2 < 3; m2++) {
-        //        for (size_t n2 = 0; n2 < 3; n2++) {
-        //            for (size_t l2 = 0; l2 < 3; l2++) {
-        //                if (m2 == 0 && n2 == 0 && l2 == 0) continue;
-        //                int m = indxs[m2];
-        //                int n = indxs[n2];
-        //                int l = indxs[l2];
-        //                size_t shift = (9 * m2 + 3 * n2 + l2) * np;
-        //                std::vector<T> shifti = {T(m) * box[0] + T(n) * box[3] + T(l) * box[6],
-        //                                         T(m) * box[1] + T(n) * box[4] + T(l) * box[7],
-        //                                         T(m) * box[2] + T(n) * box[5] + T(l) * box[8]};
+        if (use_pbc) {
+            for (size_t m2 = 0; m2 < 3; m2++) {
+                for (size_t n2 = 0; n2 < 3; n2++) {
+                    for (size_t l2 = 0; l2 < 3; l2++) {
+                        if (m2 == 0 && n2 == 0 && l2 == 0) continue;
+                        int m = indxs[m2];
+                        int n = indxs[n2];
+                        int l = indxs[l2];
+                        size_t shift = (9 * m2 + 3 * n2 + l2) * np;
+                        std::vector<T> shifti = {T(m) * box[0] + T(n) * box[3] + T(l) * box[6],
+                                                 T(m) * box[1] + T(n) * box[4] + T(l) * box[7],
+                                                 T(m) * box[2] + T(n) * box[5] + T(l) * box[8]};
 
-        //                //            std::vector<double> shifti = {(double(m) - 1) * box[0],
-        //                //                                          (double(n) - 1) * box[1],
-        //                //                                          (double(l) - 1) * box[2]};
-        //                ptc.pts[i + shift].x = xyz[i3] + shifti[0];
-        //                ptc.pts[i + shift].y = xyz[i3 + 1] + shifti[1];
-        //                ptc.pts[i + shift].z = xyz[i3 + 2] + shifti[2];
-        //            }
-        //        }
-        //    }
-        //}
+                        //            std::vector<double> shifti = {(double(m) - 1) * box[0],
+                        //                                          (double(n) - 1) * box[1],
+                        //                                          (double(l) - 1) * box[2]};
+                        ptc.pts[i + shift].x = xyz[i3] + shifti[0];
+                        ptc.pts[i + shift].y = xyz[i3 + 1] + shifti[1];
+                        ptc.pts[i + shift].z = xyz[i3 + 2] + shifti[2];
+                    }
+                }
+            }
+        }
     }
     return ptc;
 }
