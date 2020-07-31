5df25ab3ec007baefc5d41138cfc69b53de56582
Marc Riera <mrierari@ucsd.edu>
2020-07-31 08:45:41 -0700

    removed nh3 includes in energy2b

--- src/potential/2b/energy2b.cpp
+++ src/potential/2b/energy2b.cpp
@@ -87,9 +87,6 @@ double get_2b_energy(std::string mon1, std::string mon2, size_t nm, std::vector<
     } else if (mon1 == "ch4" and mon2 == "h2o") {
         x2b_A1B2Z2_C1D4_deg3_exp0::x2b_A1B2Z2_C1D4_v1x pot(mon2,mon1);
         return pot.eval(xyz2.data(), xyz1.data(), nm);
-    } else if (mon1 == "nh3" and mon2 == "nh3") {
-        mbnrg_A1B3_A1B3_deg5::mbnrg_A1B3_A1B3_deg5_v1 pot(mon1, mon2);
-        return pot.eval(xyz1.data(), xyz2.data(), nm);
     } else if (mon1 == "ar" and mon2 == "cs") {
         mbnrg_A1_B1_deg15::mbnrg_A1_B1_deg15_v1 pot(mon1, mon2);
         return pot.eval(xyz1.data(), xyz2.data(), nm);
@@ -151,9 +148,6 @@ double get_2b_energy(std::string mon1, std::string mon2, size_t nm, std::vector<
     } else if (mon1 == "ch4" and mon2 == "h2o") {
         x2b_A1B2Z2_C1D4_deg3_exp0::x2b_A1B2Z2_C1D4_v1x pot(mon2,mon1);
         energy = pot.eval(xyz2.data(), xyz1.data(), grad2.data(), grad1.data(), nm, virial);
-    } else if (mon1 == "nh3" and mon2 == "nh3") {
-        mbnrg_A1B3_A1B3_deg5::mbnrg_A1B3_A1B3_deg5_v1 pot(mon1, mon2);
-        energy =  pot.eval(xyz1.data(), xyz2.data(), grad1.data(), grad2.data(), nm, virial);
     } else if (mon1 == "ar" and mon2 == "cs") {
         mbnrg_A1_B1_deg15::mbnrg_A1_B1_deg15_v1 pot(mon1, mon2);
         energy =  pot.eval(xyz1.data(), xyz2.data(), grad1.data(), grad2.data(), nm, virial);
