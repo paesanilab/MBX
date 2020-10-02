9ecd3184b4635722d20170291fd69c1751444671
Marc Riera <mrierari@ucsd.edu>
2020-10-02 13:44:15 -0400

    Added licence text to the polynomial files for 2b, and added unittests for the polynomial functions in 2b.

--- src/potential/1b/poly_1b_A1B2_deg4_v1x.h
+++ src/potential/1b/poly_1b_A1B2_deg4_v1x.h
@@ -6,16 +6,49 @@
 #include <iomanip>
 #endif
 
+/**
+ * @file poly_1b_A1B2_deg4_v1x.h
+ * @brief Contains the structure that allows the evaluation of the polynomial for A1B2 symmetry.
+ */
+
+/**
+ * @namespace x1b_A1B2_deg4
+ * @brief Namespace that encloses the A1B2 polynomial structure
+ */
+
 namespace x1b_A1B2_deg4 {
 
 struct poly_1b_A1B2_deg4_v1x {
+    // Number of variables
     static const unsigned n_vars = 3;
+    // Number of terms
     static const unsigned size = 21;
 
+    /**
+     * @brief Evaluates the polynomial of degree 3 for A1B2 symmetry.
+     *
+     * Given the linear parameters and the value of the polynomial variables, evaluates the polynomial for the A1B2
+     * symmetry, and calculates the gradients.
+     * @param[in] a Double array of 21 elements with the linear parameters of the polynomial
+     * @param[in] x Double array of length 3 with the variable values
+     * @param[out] g Double array of length 3 that will store the gradients dP/dxi
+     * @return Value of the polynomial
+     */
     static double eval(const double a[21], const double x[3], double g[3]);
 
    public:
+    /**
+     * @brief Returns the number of variables of the polynomial
+     *
+     * @return Number of variables of the polynomial
+     */
     unsigned report_nvars() { return n_vars; };
+
+    /**
+     * @brief Returns the number of terms of the polynomial
+     *
+     * @return Number of terms of the polynomial
+     */
     unsigned report_size() { return size; };
 };
 
--- src/potential/1b/poly_1b_A1B4_deg5_exp0_v1x.h
+++ src/potential/1b/poly_1b_A1B4_deg5_exp0_v1x.h
@@ -6,18 +6,60 @@
 #include <iomanip>
 #endif
 
+/**
+ * @file poly_1b_A1B4_deg5_exp0_v1x.h
+ * @brief Contains the structure that allows the evaluation of the polynomial for A1B4 symmetry.
+ */
+
+/**
+ * @namespace x1b_A1B4_deg5_exp0
+ * @brief Namespace that encloses the A1B4 polynomial structure
+ */
+
 namespace x1b_A1B4_deg5_exp0 {
 
 struct poly_1b_A1B4_deg5_exp0_v1x {
+    // Number of variables
     static const unsigned n_vars = 10;
+    // Number of terms
     static const unsigned size = 207;
 
+    /**
+     * @brief Evaluates the polynomial of degree 5 for A1B4 symmetry.
+     *
+     * Given the linear parameters and the value of the polynomial variables, evaluates the polynomial for the A1B4
+     * symmetry
+     * @param[in] a Double array of 207 elements with the linear parameters of the polynomial
+     * @param[in] x Double array of length 10 with the variable values
+     * @return Value of the polynomial
+     */
     static double eval(const double a[207], const double x[10]);
 
+    /**
+     * @brief Evaluates the polynomial of degree 5 for A1B4 symmetry.
+     *
+     * Given the linear parameters and the value of the polynomial variables, evaluates the polynomial for the A1B4
+     * symmetry, and calculates the gradients.
+     * @param[in] a Double array of 207 elements with the linear parameters of the polynomial
+     * @param[in] x Double array of length 10 with the variable values
+     * @param[out] g Double array of length 10 that will store the gradients dP/dxi
+     * @return Value of the polynomial
+     */
     static double eval(const double a[207], const double x[10], double g[10]);
 
    public:
+    /**
+     * @brief Returns the number of variables of the polynomial
+     *
+     * @return Number of variables of the polynomial
+     */
     unsigned report_nvars() { return n_vars; };
+
+    /**
+     * @brief Returns the number of terms of the polynomial
+     *
+     * @return Number of terms of the polynomial
+     */
     unsigned report_size() { return size; };
 };
 
