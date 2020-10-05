a810dede628203de4a2460df7003e8459c252327
Marc Riera <mrierari@ucsd.edu>
2020-10-05 11:39:41 -0400

    Updated documentation in x1b classes

--- src/potential/1b/x1b_A1B2_deg4_v1x.h
+++ src/potential/1b/x1b_A1B2_deg4_v1x.h
@@ -45,29 +45,67 @@ SOFTWARE WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER RIGHTS.
 #include <cstdlib>
 #include <iomanip>
 
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file x1b_A1B2_deg4_v1x.h
+ * @brief Contains the structure of the polynomial holder for symmetry A1B2
+ */
+
+/**
+ * @namespace x1b_A1B2_deg4
+ * @brief Encloses the structure of the polynomial holder for symmetry A1B2
+ */
 
 namespace x1b_A1B2_deg4 {
 
-//----------------------------------------------------------------------------//
-
-struct x1b_A1B2_v1x {
+class x1b_A1B2_v1x {
+   public:
+    // Creates an empty class
     x1b_A1B2_v1x(){};
+
+    /**
+     * @brief Creates a class and initializes the parameters corresponding to mon
+     * @param[in] mon Monomer id of the monomer of interest
+     */
     x1b_A1B2_v1x(const std::string mon);
+
+    // Destroys the class
     ~x1b_A1B2_v1x(){};
 
+    // Polynomial for this symmetry
     typedef poly_1b_A1B2_deg4_v1x polynomial;
 
+    /**
+     * @brief Computes the one body energy for the monomers
+     *
+     * Given the coordinates of a number of monomers, it calculates the polynomial value for each one of them, and ret
+urns a vector with them.
+     * @param[in] xyz Pointer to a double array with the coordinates of the monomers.
+     * @param[in] nmon Number of monomers passed in the xyz array.
+     * @return Vector of doubles with the energies of each monomer, in the same order as the input.
+     */
     std::vector<double> eval(const double* xyz, const size_t nmon) const;
+
+    /**
+     * @brief Computes the one body energy for the monomers and their gradients
+     *
+     * Given the coordinates of a number of monomers, it calculates the polynomial value for each one of them, and ret
+urns a vector with them. The gradients are also computed.
+     * @param[in] xyz Pointer to a double array with the coordinates of the monomers.
+     * @param[in,out] grad Pointer to a double array with the gradients of the monomers.
+     * @param[in] nmon Number of monomers passed in the xyz array.
+     * @param[in,out] virial Vector of 9 doubles with the virial tensor.
+     * @return Vector of doubles with the energies of each monomer, in the same order as the input.
+     */
     std::vector<double> eval(const double* xyz, double* grad, const size_t nmon, std::vector<double>* virial = 0) const;
 
    private:
+    // Values of the non-linear parameters of the polynomials
     double d_intra_AB;
     double d_intra_BB;
     double k_intra_AB;
     double k_intra_BB;
 
-   private:
+    // Values of the linear parameters of the polynomials
     std::vector<double> coefficients;
 };
 
