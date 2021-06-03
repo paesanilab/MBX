9893c81affe96bdcb353f24021a17353885b257d
Marc Riera <mrierari@ucsd.edu>
2021-06-03 07:30:41 -0700

    Cleaned up example folder

--- plugins/repimd/examples/2h2o_1cs/equilibration/run_everything.sh
+++ plugins/repimd/examples/2h2o_1cs/equilibration/run_everything.sh
@@ -9,20 +9,41 @@ module load intel
 module unload mpi
 module load intel/mpi
 
+NREPLICAS=16
+NBEADS=1
+
 # Generate initial frames from xyz
-python3 ${MBX_HOME}/scripts/remd_analysis/pre-processing/s1-generate_initial_frames.py 100 150 16 1 input.xyz
+python3 ${MBX_HOME}/scripts/remd_analysis/pre-processing/s1-generate_initial_frames.py 100 150 $NREPLICAS $NBEADS input.xyz
 
 # Generate nrg file from xyz
 python3 ${MBX_HOME}/scripts/format_conversion/xyz2nrg.py input.xyz
 
 # Run REMD simulation
-mpirun -np 16 ${MBX_HOME}/plugins/repimd/bin/repimd -N 1 -n 10000 -p 50 -t 50 -s 0.0002 -R 15.0 -U 100.0 -i initial -f final -o output -j trajectory -T 16 -I input.nrg -J mbx.json
+mpirun -np $NREPLICAS ${MBX_HOME}/plugins/repimd/bin/repimd -N 1 -n 10000 -p 50 -t 50 -s 0.0002 -R 10.0 -U 100.0 -i initial -f final -o output -j trajectory -T $NREPLICAS -I input.nrg -J mbx.json
 
 # Unmix trajectories
-python3 /home/mrierari/codes/MBX/scripts/remd_analysis/post-processing/unmix_trajectories.py trajectory 16
+python3 /home/mrierari/codes/MBX/scripts/remd_analysis/post-processing/unmix_trajectories.py trajectory $NREPLICAS
 
 # Convert trajectories to XYZ
-for i in `seq 1 1 16`; do 
+for i in `seq 1 1 $NREPLICAS`; do 
   printf -v j "%02d" $i
-  python3 ${MBX_HOME}/scripts/remd_analysis/post-processing/traj_t_centroid_xyz.py unmixed_trajectory.$j 1
+  python3 ${MBX_HOME}/scripts/remd_analysis/post-processing/traj_t_centroid_xyz.py unmixed_trajectory.$j $NBEADS
 done
+
+# Optimize each trajectory 
+for i in `seq 1 1 $NREPLICAS`; do
+  # Optimize trajectory every 20 frames
+  rm optimized.xyz unconverged.xyz
+  printf -v j "%02d" $i
+  ${MBX_HOME}/install/bin/main/optimize  input.nrg mbx.json 500 0.000001 unmixed_trajectory.${j}.xyz 20
+  mv optimized.xyz optimized_unmixed_trajectory.${j}.xyz
+
+  # Try converging the ones that did not
+  if [ -f "unconverged.xyz" ]; then
+    ${MBX_HOME}/install/bin/main/optimize  input.nrg mbx.json 500 0.000001 unconverged.xyz 1
+    cat optimized.xyz >> optimized_unmixed_trajectory.${j}.xyz
+  fi
+  
+done
+
+
