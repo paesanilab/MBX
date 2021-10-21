e753a98f713f2ceb901c8d71d8aeebd777037f99
Marc Riera <mrierari@ucsd.edu>
2021-10-21 13:09:01 -0700

    Deleted more useless functions

--- src/potential/1b/x1b_A1B2C4_deg5_v1x.cpp
+++ src/potential/1b/x1b_A1B2C4_deg5_v1x.cpp
@@ -7,41 +7,12 @@ namespace {
 //----------------------------------------------------------------------------//
 
 struct variable {
-    double v_exp0(const double& r0, const double& k, const double* p1, const double* p2);
-
     double v_exp(const double& k, const double* p1, const double* p2);
-
-    double v_coul0(const double& r0, const double& k, const double* p1, const double* p2);
-
-    double v_coul(const double& k, const double* p1, const double* p2);
-
-    double v_gau0(const double& r0, const double& k, const double* p1, const double* p2);
 
     void grads(const double& gg, double* grd1, double* grd2, const double* p1, const double* p2);
 
     double g[3];  // diff(value, p1 - p2)
 };
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-double variable::v_gau0(const double& r0, const double& k, const double* p1, const double* p2) {
-    g[0] = p1[0] - p2[0];
-    g[1] = p1[1] - p2[1];
-    g[2] = p1[2] - p2[2];
-
-    const double r = std::sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2]);
-
-    const double exp1 = std::exp(-k * (r0 - r) * (r0 - r));
-    const double gg = 2 * k * (r0 - r) * exp1 / r;
-
-    g[0] *= gg;
-    g[1] *= gg;
-    g[2] *= gg;
-
-    return exp1;
-}
-
-//----------------------------------------------------------------------------//
 
 void variable::grads(const double& gg, double* grd1, double* grd2, const double* p1, const double* p2) {
     for (size_t i = 0; i < 3; i++) {
@@ -49,48 +20,6 @@ void variable::grads(const double& gg, double* grd1, double* grd2, const double*
         grd1[i] += d;
         grd2[i] -= d;
     }
-}
-
-//----------------------------------------------------------------------------//
-
-double variable::v_exp0(const double& r0, const double& k, const double* p1, const double* p2) {
-    g[0] = p1[0] - p2[0];
-    g[1] = p1[1] - p2[1];
-    g[2] = p1[2] - p2[2];
-
-    const double r = std::sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2]);
-
-    const double exp1 = std::exp(k * (r0 - r));
-    const double gg = -k * exp1 / r;
-
-    g[0] *= gg;
-    g[1] *= gg;
-    g[2] *= gg;
-
-    return exp1;
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-double variable::v_coul0(const double& r0, const double& k, const double* p1, const double* p2) {
-    g[0] = p1[0] - p2[0];
-    g[1] = p1[1] - p2[1];
-    g[2] = p1[2] - p2[2];
-
-    const double rsq = g[0] * g[0] + g[1] * g[1] + g[2] * g[2];
-    const double r = std::sqrt(rsq);
-
-    const double exp1 = std::exp(k * (r0 - r));
-    const double rinv = 1.0 / r;
-    const double val = exp1 * rinv;
-
-    const double gg = -(k + rinv) * val * rinv;
-
-    g[0] *= gg;
-    g[1] *= gg;
-    g[2] *= gg;
-
-    return val;
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
@@ -111,122 +40,6 @@ double variable::v_exp(const double& k, const double* p1, const double* p2) {
 
     return exp1;
 }
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-double variable::v_coul(const double& k, const double* p1, const double* p2) {
-    g[0] = p1[0] - p2[0];
-    g[1] = p1[1] - p2[1];
-    g[2] = p1[2] - p2[2];
-
-    const double rsq = g[0] * g[0] + g[1] * g[1] + g[2] * g[2];
-    const double r = std::sqrt(rsq);
-
-    const double exp1 = std::exp(k * (-r));
-    const double rinv = 1.0 / r;
-    const double val = exp1 * rinv;
-
-    const double gg = -(k + rinv) * val * rinv;
-
-    g[0] *= gg;
-    g[1] *= gg;
-    g[2] *= gg;
-
-    return val;
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-struct monomer {
-    double oh1[3];
-    double oh2[3];
-
-    void setup(const double* ohh, const double& in_plane_g, const double& out_of_plane_g, double x1[3], double x2[3]);
-
-    void grads(const double* g1, const double* g2, const double& in_plane_g, const double& out_of_plane_g,
-               double* grd) const;
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-void monomer::setup(const double* ohh, const double& in_plane_g, const double& out_of_plane_g, double* x1, double* x2) {
-    for (int i = 0; i < 3; ++i) {
-        oh1[i] = ohh[i + 3] - ohh[i];
-        oh2[i] = ohh[i + 6] - ohh[i];
-    }
-
-    const double v[3] = {oh1[1] * oh2[2] - oh1[2] * oh2[1], oh1[2] * oh2[0] - oh1[0] * oh2[2],
-                         oh1[0] * oh2[1] - oh1[1] * oh2[0]};
-
-    for (int i = 0; i < 3; ++i) {
-        const double in_plane = ohh[i] + 0.5 * in_plane_g * (oh1[i] + oh2[i]);
-        const double out_of_plane = out_of_plane_g * v[i];
-
-        x1[i] = in_plane + out_of_plane;
-        x2[i] = in_plane - out_of_plane;
-    }
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-void monomer::grads(const double* g1, const double* g2, const double& in_plane_g, const double& out_of_plane_g,
-                    double* grd) const {
-    const double gm[3] = {g1[0] - g2[0], g1[1] - g2[1], g1[2] - g2[2]};
-
-    const double t1[3] = {oh2[1] * gm[2] - oh2[2] * gm[1], oh2[2] * gm[0] - oh2[0] * gm[2],
-                          oh2[0] * gm[1] - oh2[1] * gm[0]};
-
-    const double t2[3] = {oh1[1] * gm[2] - oh1[2] * gm[1], oh1[2] * gm[0] - oh1[0] * gm[2],
-                          oh1[0] * gm[1] - oh1[1] * gm[0]};
-
-    for (int i = 0; i < 3; ++i) {
-        const double gsum = g1[i] + g2[i];
-        const double in_plane = 0.5 * in_plane_g * gsum;
-
-        const double gh1 = in_plane + out_of_plane_g * t1[i];
-        const double gh2 = in_plane - out_of_plane_g * t2[i];
-
-        grd[i + 0] += gsum - (gh1 + gh2);  // O
-        grd[i + 3] += gh1;                 // H1
-        grd[i + 6] += gh2;                 // H2
-    }
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-// struct vsites {
-//    //void TwoParticleAverageSite() {}
-//    //void ThreeParticleAverageSite() {}
-//    void OutOfPlaneSite(const double& w12, const double& w13,
-//                        const double& wcross, const double x1[3],
-//                        const double y1[3], const double y2[3],
-//                        double vs[3]);
-//    //void LocalCoordinatesSite{}
-//};
-//
-// void vsites::OutOfPlaneSite(const double& w12,
-//                            const double& w13,
-//                            const double& wcross,
-//                            const double x1[3],
-//                            const double y1[3],
-//                            const double y2[3],
-//                            double vs[3]) {
-//    double r12[3], r13[3];
-//
-//    for (int i = 0; i < 3; ++i) {
-//        r12[i] = y1[i] - x1[i];
-//        r13[i] = y2[i] - x1[i];
-//    }
-//
-//    double rc[3];
-//    rc[0] = r12[1]*r13[2] - r12[2]*r13[1];
-//    rc[1] = r12[2]*r13[0] - r12[0]*r13[2];
-//    rc[2] = r12[0]*r13[1] - r12[1]*r13[0];
-//
-//    vs[0] = x1[0] + w12 * r12[0] + w13 * r13[0] + wcross * rc[0];
-//    vs[1] = x1[1] + w12 * r12[1] + w13 * r13[1] + wcross * rc[1];
-//    vs[2] = x1[2] + w12 * r12[2] + w13 * r13[2] + wcross * rc[2];
-//}
 
 }  // namespace
 
