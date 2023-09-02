e6f80d651af5b50d92d49298eaf06a15fbfbdd7f
Henry Agnew <henryd.agnew@gmail.com>
2023-09-01 18:24:45 -0700

    Fix USER-MBX install
    The cpp files were being installed before the dependencies were, so this was what was causing the cpp files to be autoremoved.

--- plugins/lammps/USER-MBX/Install.sh
+++ plugins/lammps/USER-MBX/Install.sh
@@ -3,24 +3,27 @@
 
 mode=$1
 
-# arg1 = file, arg2 = file it depends on
 
 # enforce using portable C locale
 LC_ALL=C
 export LC_ALL
 
+# $1 = file, $2 = file it depends on
 action () {
-  if (test $mode = 0) then
+  if (test $mode = 0) then # uninstall
     rm -f ../$1
   elif (! cmp -s $1 ../$1) then
-    if (test -z "$2" || test -e ../$2) then
+    if (test -z "$2" || test -e ../$2) then # only copy if dependency exists
       cp $1 ..
-      if (test $mode = 2) then
+      if (test $mode = 1) then
+        echo "  installing src/$1"
+      else 
         echo "  updating src/$1"
       fi
     fi
-  elif (test -n "$2") then
+  elif (test -n "$2") then # remove if missing dependency
     if (test ! -e ../$2) then
+      echo "  removing src/$1 due to missing dependency src/$2"
       rm -f ../$1
     fi
   fi
@@ -28,7 +31,7 @@ action () {
 
 # list of files with optional dependencies
 
-action pair_mbx.cpp pair_mbx.h
 action pair_mbx.h
-action fix_mbx.cpp fix_mbx.h
+action pair_mbx.cpp pair_mbx.h
 action fix_mbx.h
+action fix_mbx.cpp fix_mbx.h
