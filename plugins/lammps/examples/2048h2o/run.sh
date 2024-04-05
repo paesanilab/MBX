43d74a29e0428cf948d3cf65752712c9e4cf77cf
shreya gupta <sfgupta@ucsd.edu>
2024-04-04 23:50:58 -0700

    Revert "remove untracked files"
    Delete
    
    This reverts commit 8161ffdfe367b5d536472c8e784ffe7d20320528.

--- plugins/lammps/examples/2048h2o/run.sh
+++ plugins/lammps/examples/2048h2o/run.sh
@@ -6,7 +6,7 @@ if [ $# -ne 2 ]; then
   exit
 fi
 
-MBX_HOME=$HOME/MBX-dev+LAMMPS/MBX-dev
-LAMMPS_HOME=$HOME/MBX-dev+LAMMPS
-gnv OMP_NUM_THREADS=$1 mpirun -np $2 $LAMMPS_HOME/lmp_mpi_mbx -in lammps.in > out
+MBX_HOME=$HOME/codes/MBX-dev
+LAMMPS_HOME=$HOME/codes/lammps
+env OMP_NUM_THREADS=$1 mpirun -np $2 $LAMMPS_HOME/lmp_mpi_mbx -in lammps.in > out
 
