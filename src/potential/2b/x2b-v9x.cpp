01b11112d98e36a0dbb9a6953b02834c2e95d1f6
Shishir Iyer <shiyer@ucsd.edu>
2025-03-05 14:15:27 -0800

    new vectorized code that actually does gradients correctly

--- src/potential/2b/x2b-v9x.cpp
+++ src/potential/2b/x2b-v9x.cpp
@@ -1657,9 +1657,7 @@ double x2b_v9x::eval(const double* w1, const double* w2, double* g1, double* g2,
     }
 
     double e = 0.0;
-    double t[3580*8];
-    // double t[20000];
-    // std::vector<double> e2b = poly_2b_v6x::eval(ndtd, thefit, vv, gg);
+    double t[3133*8];
     std::vector<double> e2b = poly_2b_v6x::eval(ndtd, thefit, vv, t, gg);
 
     for (size_t i = 0; i < ndtd; i++) {
