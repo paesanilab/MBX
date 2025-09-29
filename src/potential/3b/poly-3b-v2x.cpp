cfe48264b849ecff1ca1e3c3b73ed4b3ad3a6bf5
Miniland1333 <hagnew@ucsd.edu>
2025-09-29 13:48:29 -0700

    poly-3b-v2x cleaned up debug newlines

--- src/potential/3b/poly-3b-v2x.cpp
+++ src/potential/3b/poly-3b-v2x.cpp
@@ -27121,22 +27121,18 @@ Entering " << __func__ << " in " << __FILE__ << std::endl;
 
 #ifdef DEBUG
     std::cerr << std::scientific << std::setprecision(20);
-    std::cerr << "
-Exiting " << __func__ << " in " << __FILE__ << std::endl;
-    std::cerr << "Input linear constants (a):
-";
+    std::cerr << "Exiting " << __func__ << " in " << __FILE__ << std::endl;
+    std::cerr << "Input linear constants (a):\n";
     for (size_t j = 0; j < 1163; j++) {
         std::cerr << a[j] << " , ";
     }
     std::cerr << std::endl;
-    std::cerr << "Input polynomial variables (x):
-";
+    std::cerr << "Input polynomial variables (x):\n";
     for (size_t j = 0; j < 36; j++) {
         std::cerr << x[j] << " , ";
     }
     std::cerr << std::endl;
-    std::cerr << "Output polynomial gradients (g):
-";
+    std::cerr << "Output polynomial gradients (g):\n";
     for (size_t j = 0; j < 36; j++) {
         std::cerr << g[j] << " , ";
     }
