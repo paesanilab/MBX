6229485219f13faca6059a24fdbfe888995e799b
EthanBullVulpe <ebullvul@ucsd.edu>
2026-02-03 16:42:21 -0800

    added smaller 3b pip

--- src/potential/3b/x3b-v2x.cpp
+++ src/potential/3b/x3b-v2x.cpp
@@ -1467,13 +1467,13 @@ double x3b_v2x::eval(const double* w1, const double* w2, const double* w3, doubl
 
     
 
-    double bigmem[3460*8];
+    double bigmem[1660*8];
 
     void* pool = reinterpret_cast<void *>(bigmem);
 
-    size_t space = 3460*8*8;
+    size_t space = 1660*8*8;
 
-    double* t = reinterpret_cast<double *>(std::align(128, 3449*8*8, pool, space));
+    double* t = reinterpret_cast<double *>(std::align(128, 1635*8*8, pool, space));
 
     double x[36*8];
     double gg[36*8];
