9ad84fd9fa468020aa4194763cf2723fa33aab86
EthanBullVulpe <ebullvul@ucsd.edu>
2026-02-19 17:04:04 -0800

    reverted lammps build

--- plugins/lammps/USER-MBX/fix_mbx.h
+++ plugins/lammps/USER-MBX/fix_mbx.h
@@ -1,7 +1,7 @@
 /* -*- c++ -*- ----------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   https://www.lammps.org/, Sandia National Laboratories
-   LAMMPS Development team: developers@lammps.org
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
@@ -13,7 +13,7 @@
 
 #ifdef FIX_CLASS
 
-FixStyle(MBX, FixMBX)
+FixStyle(mbx, FixMBX)
 
 #else
 
@@ -22,161 +22,192 @@ FixStyle(MBX, FixMBX)
 
 #include "fix.h"
 
+// MBX
 
+#include "bblock/system.h"
+
+enum {
+    MBXT_INIT = 0,
+    MBXT_UPDATE_XYZ,
+    MBXT_INIT_FULL,
+    MBXT_UPDATE_XYZ_FULL,
+    MBXT_INIT_LOCAL,
+    MBXT_UPDATE_XYZ_LOCAL,
+    MBXT_E1B,
+    MBXT_E2B_LOCAL,
+    MBXT_E2B_GHOST,
+    MBXT_E3B_LOCAL,
+    MBXT_E3B_GHOST,
+    MBXT_E4B_LOCAL,
+    MBXT_E4B_GHOST,
+    MBXT_DISP,
+    MBXT_DISP_PME,
+    MBXT_BUCK,
+    MBXT_ELE,
+    MBXT_ACCUMULATE_F,
+    MBXT_ACCUMULATE_F_FULL,
+    MBXT_ACCUMULATE_F_LOCAL,
+
+    MBXT_ELE_PERMDIP_REAL,
+    MBXT_ELE_PERMDIP_PME,
+
+    MBXT_ELE_DIPFIELD_REAL,
+    MBXT_ELE_DIPFIELD_PME,
+
+    MBXT_ELE_GRAD_REAL,
+    MBXT_ELE_GRAD_PME,
+    MBXT_ELE_GRAD_FIN,
+
+    MBXT_ELE_COMM_REVFOR,
+    MBXT_ELE_COMM_REVSET,
+    MBXT_ELE_COMM_REV,
+    MBXT_ELE_COMM_FORSET,
+    MBXT_ELE_COMM_FOR,
+
+    MBXT_ELE_PME_SETUP,
+    MBXT_ELE_PME_C,
+    MBXT_ELE_PME_D,
+    MBXT_ELE_PME_E,
+
+    MBXT_DISP_PME_SETUP,
+    MBXT_DISP_PME_E,
+
+    MBXT_NUM_TIMERS
+};
 
 namespace LAMMPS_NS {
 
 class FixMBX : public Fix {
-  friend class PairMBX;
+    friend class PairMBX;
 
- public:
-  FixMBX(class LAMMPS *, int, char **);
-  ~FixMBX();
-  int setmask();
-  virtual void post_constructor();
-  virtual void init();
-  virtual void init_storage();
-  void setup(int);
-  void min_setup(int);
-  virtual void setup_post_neighbor();
-  virtual void post_neighbor();
-  void min_post_neighbor();
-  void setup_pre_force(int);
-  virtual void pre_force(int);
+   public:
+    FixMBX(class LAMMPS *, int, char **);
+    ~FixMBX();
+    int setmask();
+    virtual void post_constructor();
+    virtual void init();
+    virtual void init_storage();
+    void setup(int);
+    void min_setup(int);
+    virtual void setup_post_neighbor();
+    virtual void post_neighbor();
+    void setup_pre_force(int);
+    virtual void pre_force(int);
 
-  void min_setup_pre_force(int);
-  void min_pre_force(int);
+    void min_setup_pre_force(int);
+    void min_pre_force(int);
 
-  void setup_pre_exchange();
-  void pre_exchange();
+    void setup_pre_exchange();
+    void pre_exchange();
 
-  void post_force(int);
-  void min_post_force(int);
+    void post_force(int);
 
- protected:
-  struct MBXImpl *mbx_impl;
-  class PairMBX *pair_mbx;    // pointer to MBX pair_style
+   protected:
+    class PairMBX *pair_mbx;  // pointer to MBX pair_style
 
-  static std::string cite_pair_mbx;
-  int me, nprocs;
-  bigint ngroup;
+    bblock::System *ptr_mbx;        // pointer to MBX object
+    bblock::System *ptr_mbx_full;   // pointer to MBX object for full system
+    bblock::System *ptr_mbx_local;  // pointer to MBX object for local atoms
 
-  bool mbx_aspc_enabled;
-  bool print_dipoles;
+    int me, nprocs;
+    bigint ngroup;
 
-  bool first_step;
-  bool has_gcmc;
+    bool mbx_mpi_enabled;
+    bool mbx_aspc_enabled;
+    bool print_dipoles;
 
-  int use_json;
-  std::string json_settings;
+    bool first_step;
+    bool has_gcmc;
 
-  int print_verbose;
+    int use_json;
+    char *json_file;
+    std::string json_settings;
 
-  int num_mol_types;    // # of unique molecule types
-  int num_molecules;    // total # of molecules
-  int *num_atoms_per_mol;                // array of # of atoms per molecule for each type
-  int *lower_atom_type_index_in_mol;     // array with the lowest atom type index in the monomer
-  int *higher_atom_type_index_in_mol;    // array with the highest atom type index in the monomer
-  int **order_in_mol;                    // array with the atom order for each monomer
-  char **mol_names;    // array of molecule names
+    int print_settings;
 
-  int *mol_type;      // per-atom array of molecule type
-  int *mol_anchor;    // per-atom array 1/0 if anchor atom of a molecule
-  int *mol_local;    // per-molecule array 1/0 if molecule has at least one local particle
+    int num_mol_types;  // # of unique molecule types
+    int num_molecules;  // total # of molecules
+    // int *num_mols;           // array of # of molecules of each type
+    int *num_atoms_per_mol;              // array of # of atoms per molecule for each type
+    int *lower_atom_type_index_in_mol;   // array with the lowest atom type index in the monomer
+    int *higher_atom_type_index_in_mol;  // array with the highest atom type index in the monomer
+    int **order_in_mol;                  // array with the atom order for each monomer
+    // tagint *mol_offset;      // array of atom count per molecule prefix sum
+    char **mol_names;  // array of molecule names
 
-  int mbx_num_atoms, mbx_num_ext;
-  int mbx_num_atoms_local, mbx_num_ext_local;
+    int *mol_type;    // per-atom array of molecule type
+    int *mol_anchor;  // per-atom array 1/0 if anchor atom of a molecule
+    // int *mol_order;   // per-atom array 1/2/3/4... with position of atom in molecule
+    int *mol_local;   // per-molecule array 1/0 if molecule has at least one local particle
 
-  int *mbxt_count;
-  double *mbxt_time;
-  double *mbxt_time_start;
-  double mbxt_initial_time;
-  void mbxt_start(int);
-  void mbxt_stop(int);
-  void mbxt_write_summary();
-  void mbxt_print_time(const char *, int, double *);
-  enum MBXT_LABELS : int {
-    INIT = 0,
-    UPDATE_XYZ,
-    INIT_LOCAL,
-    UPDATE_XYZ_LOCAL,
-    E1B,
-    E2B_GHOST,
-    E3B_GHOST,
-    E4B_GHOST,
-    DISP,
-    DISP_PME,
-    BUCK,
-    ELE,
-    ACCUMULATE_F,
-    ACCUMULATE_F_LOCAL,
+    int mbx_num_atoms, mbx_num_ext;
+    int mbx_num_atoms_full, mbx_num_ext_full;
+    int mbx_num_atoms_local, mbx_num_ext_local;
 
-    ELE_PERMDIP_REAL,
-    ELE_PERMDIP_PME,
+    int *mbxt_count;
+    double *mbxt_time;
+    double *mbxt_time_start;
+    double mbxt_initial_time;
+    void mbxt_start(int);
+    void mbxt_stop(int);
+    void mbxt_write_summary();
+    void mbxt_print_time(const char *, int, double *);
 
-    ELE_DIPFIELD_REAL,
-    ELE_DIPFIELD_PME,
+    int aspc_order;
+    int aspc_num_hist;
+    int aspc_max_num_hist;
+    int aspc_per_atom_size;
+    double **aspc_dip_hist;
 
-    ELE_GRAD_REAL,
-    ELE_GRAD_PME,
-    ELE_GRAD_FIN,
+    int aspc_step;
+    int aspc_step_reset;
 
-    ELE_COMM_REVFOR,
-    ELE_COMM_REVSET,
-    ELE_COMM_REV,
-    ELE_COMM_FORSET,
-    ELE_COMM_FOR,
+    double **mbx_dip;
 
-    ELE_PME_SETUP,
-    ELE_PME_C,
-    ELE_PME_D,
-    ELE_PME_E,
+    // rank 0's copy of all atoms in simulation cell
 
-    DISP_PME_SETUP,
-    DISP_PME_E,
+    int *mol_anchor_full;
+    int *mol_type_full;
+    double **x_full;
+    double **f_full;
+    double **f_local;
+    tagint *tag_full;
+    int *atom_map_full;
+    int *nlocal_rank;
+    int *nlocal_disp;
+    int *nlocal_rank3;
+    int *nlocal_disp3;
 
-    NUM_TIMERS
-  };
+    void mbx_init();
+    void mbx_init_full();
+    void mbx_init_local();
 
-  bool validateMBXFixParameters(int narg, char **arg);
+    void mbx_update_xyz();
+    void mbx_update_xyz_full();
+    void mbx_update_xyz_local();
 
-  int aspc_order;
-  int aspc_num_hist;
-  int aspc_max_num_hist;
-  int aspc_per_atom_size;
-  double **aspc_dip_hist;
+    void mbx_fill_system_information_from_atom();
 
-  int aspc_step;
-  int aspc_step_reset;
+    void mbx_init_dipole_history_local();
 
-  double **mbx_dip;
+    void mbx_get_dipoles_local();
 
-  void mbx_init();
-  void mbx_init_local();
+    int get_num_atoms_per_monomer(char *, bool &);
+    int get_include_monomer(char *, int, bool &, bool &);
+    void add_monomer_atom_types(char *, std::vector<std::string> &);
 
-  void mbx_update_xyz();
-  void mbx_update_xyz_local();
-
-  void mbx_fill_system_information_from_atom();
-
-  void mbx_init_dipole_history_local();
-
-  void mbx_get_dipoles_local();
-
-  int get_num_atoms_per_monomer(char *, bool &);
-  int get_include_monomer(char *, int, bool &, bool &);
-  void add_monomer_atom_types(char *, std::vector<std::string> &);
-  std::pair<int, int> parse_dp1_range(const std::string &);
-
-  virtual int pack_forward_comm(int, int *, double *, int, int *);
-  virtual void unpack_forward_comm(int, int, double *);
-  virtual void grow_arrays(int);
-  virtual void copy_arrays(int, int, int);
-  virtual int pack_exchange(int, double *);
-  virtual int unpack_exchange(int, double *);
+    virtual int pack_forward_comm(int, int *, double *, int, int *);
+    virtual void unpack_forward_comm(int, int, double *);
+    virtual int pack_reverse_comm(int, int, double *);
+    virtual void unpack_reverse_comm(int, int *, double *);
+    virtual double memory_usage();
+    virtual void grow_arrays(int);
+    virtual void copy_arrays(int, int, int);
+    virtual int pack_exchange(int, double *);
+    virtual int unpack_exchange(int, double *);
 };
 
-}    // namespace LAMMPS_NS
+}  // namespace LAMMPS_NS
 
 #endif
 #endif
--- plugins/lammps/USER-MBX/pair_mbx.h
+++ plugins/lammps/USER-MBX/pair_mbx.h
@@ -1,7 +1,7 @@
 /* -*- c++ -*- ----------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   https://www.lammps.org/, Sandia National Laboratories
-   LAMMPS Development team: developers@lammps.org
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
@@ -21,50 +21,78 @@ PairStyle(mbx, PairMBX)
 #define LMP_PAIR_MBX_H
 
 #include "pair.h"
+#include "fix_mbx.h"
 
+// MBX
+
+#include "bblock/system.h"
 
 namespace LAMMPS_NS {
-  class FixMBX;
 
 class PairMBX : public Pair {
-  friend FixMBX;    // accesses cut_global
+    friend FixMBX;  // accesses cut_global
 
- public:
-  PairMBX(class LAMMPS *);
-  virtual ~PairMBX();
-  virtual void compute(int, int);
-  void settings(int, char **);
-  void coeff(int, char **);
-  double init_one(int, int);
+   public:
+    PairMBX(class LAMMPS *);
+    virtual ~PairMBX();
+    virtual void compute(int, int);
+    void settings(int, char **);
+    void coeff(int, char **);
+    void init_style();
+    double init_one(int, int);
+    void write_data(FILE *);
+    void write_data_all(FILE *);
+    void *extract(const char *, int &);
 
- protected:
-  double cut_global;
+   protected:
+    double cut_global;
+    double **cut;
 
-  int me;
+    int me;
 
-  FixMBX *fix_MBX;    // owner of MBX objects
+    FixMBX *fix_mbx;  // owner of MBX objects
 
-  int nmolecule;    // # of molecules in system (would break if number of molecules can change)
+    int nmolecule;  // # of molecules in system (would break if number of molecules can change)
 
-  double mbx_e1b;
-  double mbx_e2b;
-  double mbx_e3b;
-  double mbx_e4b;
-  double mbx_disp;
-  double mbx_buck;
-  double mbx_ele;
-  double mbx_total_energy;
+    double mbx_e1b;
+    double mbx_e2b;
+    double mbx_e3b;
+    double mbx_e4b;
+    double mbx_disp;
+    double mbx_buck;
+    double mbx_ele;
+    double mbx_total_energy;
 
-  double mbx_virial[6];
+    double mbx_virial[6];
 
-  virtual void allocate();
+    virtual void allocate();
 
-  void accumulate_f(bool);
-  void accumulate_f_all(bool);    // local + ghost
-  void accumulate_f_local(bool);
+    void accumulate_f(bool);
+    void accumulate_f_all(bool);  // local + ghost
+    void accumulate_f_full(bool);
+    void accumulate_f_local(bool);
 };
 
-}    // namespace LAMMPS_NS
+}  // namespace LAMMPS_NS
 
 #endif
 #endif
+
+    /* ERROR/WARNING messages:
+
+    E: Illegal ... command
+
+    Self-explanatory.  Check the input script syntax and compare to the
+    documentation for the command.  You can use -echo screen as a
+    command-line option when running LAMMPS to see the offending line.
+
+    E: Incorrect args for pair coefficients
+
+    Self-explanatory.  Check the input script or data file.
+
+    E: Pair cutoff < Respa interior cutoff
+
+    One or more pairwise cutoffs are too short to use with the specified
+    rRESPA cutoffs.
+
+    */
