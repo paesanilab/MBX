950d2a95b40825fc69495ab38a6c9bd89bb7e39f
Miniland1333 <hagnew@ucsd.edu>
2026-02-26 17:39:29 -0800

    Added back hardcodedd MY_PI

--- src/kdtree/nanoflann.hpp
+++ src/kdtree/nanoflann.hpp
@@ -51,11 +51,12 @@
 #include <algorithm>
 #include <stdexcept>
 #include <cstdio>          // for fwrite()
-#define _USE_MATH_DEFINES  // Required by MSVC to define constants::MY_PI,etc. in <cmath>
 #include <cmath>           // for abs()
 #include <cstdlib>         // for abs()
 #include <limits>
 #include "tools/constants.h"
+
+static constexpr double MY_PI = 3.14159265358979323846;           // pi
 
 // Avoid conflicting declaration of min/max macros in windows headers
 #if !defined(NOMINMAX) && (defined(_WIN32) || defined(_WIN32_) || defined(WIN32) || defined(_WIN64))
@@ -395,10 +396,10 @@ struct SO2_Adaptor {
     inline DistanceType accum_dist(const U a, const V b, int) const {
         DistanceType result = DistanceType();
         result = b - a;
-        if (result > constants::MY_PI)
-            result -= 2. * constants::MY_PI;
-        else if (result < -constants::MY_PI)
-            result += 2. * constants::MY_PI;
+        if (result > MY_PI)
+            result -= 2. * MY_PI;
+        else if (result < -MY_PI)
+            result += 2. * MY_PI;
         return result;
     }
 };
