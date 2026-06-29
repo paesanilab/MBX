9e49ba2601e133fe70b0514eed4d5e6f97192e1d
Suman Saha <24.sumansaha@gmail.com>
2026-06-29 08:51:04 -0700

    feat(nma): implement optional NMA potential compilation under --enable-potential-nma

--- src/potential/2b/energy2b.cpp
+++ src/potential/2b/energy2b.cpp
@@ -192,8 +192,12 @@ double get_2b_energy(std::string mon1, std::string mon2, size_t nm, std::vector<
         // =====>> BEGIN SECTION 2B_NO_GRADIENT <<=====
         // =====>> PASTE YOUR CODE BELOW <<=====
     } else if (mon1 == "nma" and mon2 == "nma") {
+#ifdef HAVE_POTENTIAL_NMA
         mbnrg_A1B1C1D3E1F1G1H3_A1B1C1D3E1F1G1H3_deg2::mbnrg_A1B1C1D3E1F1G1H3_A1B1C1D3E1F1G1H3_deg2_v1 pot(mon1, mon2);
         return pot.eval(xyz1.data(), xyz2.data(), nm);
+#else
+        throw std::runtime_error("Monomer nma is not compiled in this version of the code. Please reconfigure with --enable-potential-nma to use it.");
+#endif
 
         // =====>> END SECTION 2B_NO_GRADIENT <<=====
 
@@ -377,8 +381,12 @@ double get_2b_energy(std::string mon1, std::string mon2, size_t nm, std::vector<
         // =====>> BEGIN SECTION 2B_GRADIENT <<=====
         // ====>> PASTE YOUR CODE BELOW <<====
     } else if (mon1 == "nma" and mon2 == "nma") {
+#ifdef HAVE_POTENTIAL_NMA
         mbnrg_A1B1C1D3E1F1G1H3_A1B1C1D3E1F1G1H3_deg2::mbnrg_A1B1C1D3E1F1G1H3_A1B1C1D3E1F1G1H3_deg2_v1 pot(mon1, mon2);
         energy =  pot.eval(xyz1.data(), xyz2.data(), grad1.data(), grad2.data(), nm, virial);
+#else
+        throw std::runtime_error("Monomer nma is not compiled in this version of the code. Please reconfigure with --enable-potential-nma to use it.");
+#endif
 
         // =====>> END SECTION 2B_GRADIENT <<=====
     } else {
